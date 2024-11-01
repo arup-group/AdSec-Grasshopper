58e34ea2d4f9861c0a4eaad8f7346f482575bde5
spsarras <psarras.st@gmail.com>
2024-11-01 23:43:52 +0000

    feat: added material parameter

---
+++ TestGrasshopperObjects/MalformedBusiness.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Oasys.Business;
+
+using Attribute = Oasys.Business.Attribute;
+
+namespace AdSecGHTests.Helpers {
+  public class MalformedBusiness : IBusinessComponent {
+
+    public MalformedParameter Malformed { get; set; } = new MalformedParameter {
+      Name = "Malformed",
+      NickName = "M",
+      Description = "Malformed description",
+    };
+    public ComponentAttribute Metadata { get; set; } = new ComponentAttribute {
+      Name = "Malformed Component",
+      NickName = "MC",
+      Description = "Malformed Component",
+    };
+    public ComponentOrganisation Organisation { get; set; } = new ComponentOrganisation {
+      Category = "Test",
+      SubCategory = "Test",
+    };
+
+    public Attribute[] GetAllInputAttributes() {
+      return new Attribute[] {
+        Malformed,
+      };
+    }
+
+    public Attribute[] GetAllOutputAttributes() {
+      return new Attribute[] {
+        Malformed,
+      };
+    }
+
+    public void UpdateInputValues(params object[] values) { throw new NotImplementedException(); }
+
+    public void Compute() { }
+  }
+}
