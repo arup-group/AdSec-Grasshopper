3c74f9b6c9cf093949f8332f7b4ad4ea25f82c95
spsarras <psarras.st@gmail.com>
2024-11-08 17:08:28 +0000

    test: all outputs from flattenRebar

--- AdSecCore/Constants/ContextUnits.cs
+++ AdSecCore/Constants/ContextUnits.cs
@@ -22,6 +22,18 @@ namespace AdSecGHCore.Constants {
       get => CurrentUnits.LengthUnitGeometry;
       set => CurrentUnits.LengthUnitGeometry = value;
     }
+    public ForceUnit ForceUnit {
+      get => CurrentUnits.ForceUnit;
+      set => CurrentUnits.ForceUnit = value;
+    }
+    public StrainUnit StrainUnit {
+      get => CurrentUnits.StrainUnit;
+      set => CurrentUnits.StrainUnit = value;
+    }
+    public PressureUnit PressureUnit {
+      get => CurrentUnits.PressureUnit;
+      set => CurrentUnits.PressureUnit = value;
+    }
 
     public void SetDefaultUnits() {
       CurrentUnits = new DefaultUnitSet();
@@ -31,11 +43,17 @@ namespace AdSecGHCore.Constants {
   public class DefaultUnitSet : UnitSet {
 
     public LengthUnit LengthUnitGeometry { get; set; } = LengthUnit.Meter;
+    public ForceUnit ForceUnit { get; set; } = ForceUnit.Kilonewton;
+    public StrainUnit StrainUnit { get; set; } = StrainUnit.Ratio;
+    public PressureUnit PressureUnit { get; set; } = PressureUnit.Megapascal;
   }
 
   public interface UnitSet {
 
     LengthUnit LengthUnitGeometry { get; set; }
+    ForceUnit ForceUnit { get; set; }
+    StrainUnit StrainUnit { get; set; }
+    PressureUnit PressureUnit { get; set; }
   }
 
 }
