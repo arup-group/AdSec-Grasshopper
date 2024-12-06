acba464d8f495f879eb2aabfba4cf42c42fd44a1
spsarras <psarras.st@gmail.com>
2024-12-06 10:38:31 +0000

    test: CreatePoint Function

---
+++ AdSecCore/Functions/PointRebarFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using AdSecGHCore.Constants;
+
+namespace AdSecCore.Functions {
+  public class PointRebarFunction : IFunction {
+    public FuncAttribute Metadata { get; set; } = new FuncAttribute {
+      Name = "Create Vertex Point",
+      NickName = "Vertex Point",
+      Description = "Create a 2D vertex in local yz-plane for AdSec Profile and Reinforcement",
+    };
+    public Organisation Organisation { get; set; } = new Organisation {
+      Category = CategoryName.Name(),
+      SubCategory = SubCategoryName.Cat3(),
+      Hidden = false,
+    };
+    public Attribute[] GetAllInputAttributes() { throw new NotImplementedException(); }
+
+    public Attribute[] GetAllOutputAttributes() { throw new NotImplementedException(); }
+
+    public void Compute() { throw new NotImplementedException(); }
+  }
+}
---
+++ AdSecCoreTests/Functions/PointRebarTests.cs
@@ -0,0 +1,28 @@
+﻿using AdSecCore.Functions;
+
+using AdSecGHCore.Constants;
+
+namespace AdSecCoreTests.Functions {
+  public class PointRebarTests {
+    private readonly PointRebarFunction pointRebarFunction;
+
+    public PointRebarTests() {
+      pointRebarFunction = new PointRebarFunction();
+    }
+
+    [Fact]
+    public void ShouldStartHidden() {
+      Assert.False(pointRebarFunction.Organisation.Hidden);
+    }
+
+    [Fact]
+    public void ShouldBeOnSubCat3() {
+      Assert.Equal(SubCategoryName.Cat3(), pointRebarFunction.Organisation.SubCategory);
+    }
+
+    [Fact]
+    public void ShouldBeOnCatName() {
+      Assert.Equal(CategoryName.Name(), pointRebarFunction.Organisation.Category);
+    }
+  }
+}
