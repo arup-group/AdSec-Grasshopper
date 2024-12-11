0e8f875cc77b01b1d5cf16c7d3ce5997879de544
DominikaLos <dominika.los@arup.com>
2024-12-11 11:19:25 +0100

    refactor: added tests for ighParam extensions

--- AdSecGHTests/Helpers/IGHParamExtensionsTests.cs
+++ AdSecGHTests/Helpers/IGHParamExtensionsTests.cs
@@ -1,3 +1,35 @@
-﻿namespace AdSecGHTests.Helpers {
-  public class IGHParamExtensionsTests { }
+﻿using AdSecGH.Components;
+using AdSecGH.Helpers;
+
+using Grasshopper.Kernel;
+
+using Xunit;
+
+namespace AdSecGHTests.Helpers {
+  [Collection("GrasshopperFixture collection")]
+  public class IGHParamExtensionsTests {
+    private GH_Component owner;
+
+    public IGHParamExtensionsTests() {
+      owner = new CreatePoint(); // whatever component
+    }
+
+    [Fact]
+    public void ConvertErrorShouldAddErrorIntoParam() {
+      owner.Params.Input[0].ConvertError("test");
+
+      var actualResult = owner.RuntimeMessages(GH_RuntimeMessageLevel.Error);
+      Assert.Single(actualResult);
+      Assert.Contains("test", actualResult[0]);
+    }
+
+    [Fact]
+    public void FailedToCollectDataWarningShouldAddWarningIntoParam() {
+      owner.Params.Input[0].FailedToCollectDataWarning();
+
+      var actualResult = owner.RuntimeMessages(GH_RuntimeMessageLevel.Warning);
+      Assert.Single(actualResult);
+      Assert.Contains("failed", actualResult[0]);
+    }
+  }
 }
