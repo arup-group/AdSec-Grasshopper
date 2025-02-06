c3ca3ae9701a2091061fcd72b9762eb8fc767ca3
Sandeep Kumar <sandeep.kumar@arup.com>
2025-02-06 13:15:40 +0530

    ADSECGH-89: test major and minor range

--- AdSecGHTests/Helpers/DiagramTest.cs
+++ AdSecGHTests/Helpers/DiagramTest.cs
@@ -15,6 +15,24 @@ namespace AdSecGHTests.Helpers {
     }
 
     [Fact]
+    public void MajorRangeIsExpected() {
+      GridAxis gridAxis = new GridAxis(5, -1);
+      int i = 0;
+      foreach (float range in gridAxis.MajorRange) {
+        Assert.Equal(gridAxis.min_value + gridAxis.major_step * i++, range);
+      }
+    }
+
+    [Fact]
+    public void MinorRangeIsExpected() {
+      GridAxis gridAxis = new GridAxis(5, -1);
+      int i = 0;
+      foreach (float range in gridAxis.MinorRange) {
+        Assert.Equal(gridAxis.min_value + gridAxis.minor_step * i++, range);
+      }
+    }
+
+    [Fact]
     public void StepIsWithinExpectedRangeWhenScalingIsLessThanTwoPointFive() {
       GridAxis gridAxis = new GridAxis(200, -20);
       Assert.Equal(20, gridAxis.major_step);
@@ -41,5 +59,6 @@ namespace AdSecGHTests.Helpers {
       Assert.Equal(200, gridAxis.major_step);
       Assert.Equal(50, gridAxis.minor_step);
     }
+
   }
 }
