8a8a0fd768d5493df8c0643646990345c8629fb5
Sandeep Kumar <sandeep.kumar@arup.com>
2025-02-10 19:43:18 +0530

    ADSECGH-89: use precision to avoid random failure

--- AdSecGHTests/Helpers/DiagramTest.cs
+++ AdSecGHTests/Helpers/DiagramTest.cs
@@ -10,8 +10,8 @@ namespace AdSecGHTests.Helpers {
     [Fact]
     public void StepIsExpectedWhenMinimumAndMaximumValueAreSame() {
       GridAxis gridAxis = new GridAxis(100, 100);
-      Assert.Equal(0.02, gridAxis.major_step);
-      Assert.Equal(0.05, gridAxis.minor_step);
+      Assert.Equal(0.02, gridAxis.major_step, 5);
+      Assert.Equal(0.05, gridAxis.minor_step, 5);
     }
 
     [Fact]
@@ -19,7 +19,7 @@ namespace AdSecGHTests.Helpers {
       GridAxis gridAxis = new GridAxis(5, -1);
       int i = 0;
       foreach (float range in gridAxis.MajorRange) {
-        Assert.Equal(gridAxis.min_value + gridAxis.major_step * i++, range);
+        Assert.Equal(gridAxis.min_value + gridAxis.major_step * i++, range, 5);
       }
     }
 
@@ -28,36 +28,36 @@ namespace AdSecGHTests.Helpers {
       GridAxis gridAxis = new GridAxis(5, -1);
       int i = 0;
       foreach (float range in gridAxis.MinorRange) {
-        Assert.Equal(gridAxis.min_value + gridAxis.minor_step * i++, range);
+        Assert.Equal(gridAxis.min_value + gridAxis.minor_step * i++, range, 5);
       }
     }
 
     [Fact]
     public void StepIsWithinExpectedRangeWhenScalingIsLessThanTwoPointFive() {
       GridAxis gridAxis = new GridAxis(200, -20);
-      Assert.Equal(20, gridAxis.major_step);
-      Assert.Equal(5, gridAxis.minor_step);
+      Assert.Equal(20, gridAxis.major_step, 5);
+      Assert.Equal(5, gridAxis.minor_step, 5);
     }
 
     [Fact]
     public void StepIsWithinExpectedRangeWhenScalingIsLessThanFive() {
       GridAxis gridAxis = new GridAxis(450, -20);
-      Assert.Equal(50, gridAxis.major_step);
-      Assert.Equal(10, gridAxis.minor_step);
+      Assert.Equal(50, gridAxis.major_step, 5);
+      Assert.Equal(10, gridAxis.minor_step, 5);
     }
 
     [Fact]
     public void StepIsWithinExpectedRangeWhenScalingIsLessThanSevenPointFive() {
       GridAxis gridAxis = new GridAxis(700, -20);
-      Assert.Equal(100, gridAxis.major_step);
-      Assert.Equal(20, gridAxis.minor_step);
+      Assert.Equal(100, gridAxis.major_step, 5);
+      Assert.Equal(20, gridAxis.minor_step, 5);
     }
 
     [Fact]
     public void StepIsWithinExpectedRangeWhenScalingIsGreaterThanSevenPointFive() {
       GridAxis gridAxis = new GridAxis(800, -20);
-      Assert.Equal(200, gridAxis.major_step);
-      Assert.Equal(50, gridAxis.minor_step);
+      Assert.Equal(200, gridAxis.major_step, 5);
+      Assert.Equal(50, gridAxis.minor_step, 5);
     }
 
   }
