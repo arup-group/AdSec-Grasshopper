3eac3c7b22c27b9ddc6b6f97e0974b8a52611c8c
SandeepArup <61278421+SandeepArup@users.noreply.github.com>
2025-02-14 14:11:21 +0530

    ADSECGH-89:Increase coverage for Diagram class (#117)
    * ADSECGH-89:Increase coverage for Diagram class
    
    * ADSECGH-89: test major and minor range
    
    * ADSECGH-89 : NM,MM chart test
    
    * ADSECGH-89: use precision to avoid random failure
    
    * ADSECGH-89: move common implementation in function
    
    * ADSECGH-89: revert unwanted changes
    
    * ADSECGH-89: remove experimental code
    
    * ADSECGH-89: remove unused code
    
    * ADSECGH-89: fix test failure
    
    * ADSECGH-89: update code to resolve review comment
    
    * ADSECGH-89: meaningful name for variable
    
    * ADSECGH-89: missing test for geometry and CastTo
    
    * ADSECGH-89: refactor code
    
    * ADSECGH-89: fix for failing test
    
    * ADSECGH-89: additional test for grid axis
    
    * ADSECGH-89: test updated as per review comment
    
    * ADSECGH-89: test for test function
    
    * ADSECGH-89: test tostring function

---
+++ AdSecGHTests/Helpers/DiagramTest.cs
@@ -0,0 +1,85 @@
+﻿
+using Xunit;
+
+using static AdSecGH.Helpers.Diagram;
+
+namespace AdSecGHTests.Helpers {
+  [Collection("GrasshopperFixture collection")]
+  public class DiagramTest {
+
+    [Fact]
+    public void StepIsInfinityWhenMaximumAndMinimumValueAreUsed() {
+      GridAxis gridAxis = new GridAxis(float.MaxValue, float.MinValue);
+      Assert.True(float.IsInfinity(gridAxis.major_step));
+      Assert.True(float.IsInfinity(gridAxis.minor_step));
+    }
+
+    [Fact]
+    public void StepIsInfinityWhenWhenMinimumAndMaximumValueAreUsed() {
+      GridAxis gridAxis = new GridAxis(float.MinValue, float.MaxValue);
+      Assert.True(float.IsInfinity(gridAxis.major_step));
+      Assert.True(float.IsInfinity(gridAxis.minor_step));
+    }
+
+    [Fact]
+    public void StepIsExpectedWhenMinimumAndMaximumValueAreZero() {
+      GridAxis gridAxis = new GridAxis(0, 0);
+      Assert.Equal(0.2, gridAxis.major_step, 5);
+      Assert.Equal(0.05, gridAxis.minor_step, 5);
+    }
+
+    [Fact]
+    public void StepIsExpectedWhenMinimumAndMaximumValueAreSame() {
+      GridAxis gridAxis = new GridAxis(100, 100);
+      Assert.Equal(0.2, gridAxis.major_step, 5);
+      Assert.Equal(0.05, gridAxis.minor_step, 5);
+    }
+
+    [Fact]
+    public void MajorRangeIsExpected() {
+      GridAxis gridAxis = new GridAxis(5, -1);
+      int i = 0;
+      foreach (float range in gridAxis.MajorRange) {
+        Assert.Equal(gridAxis.min_value + gridAxis.major_step * i++, range, 5);
+      }
+    }
+
+    [Fact]
+    public void MinorRangeIsExpected() {
+      GridAxis gridAxis = new GridAxis(5, -1);
+      int i = 0;
+      foreach (float range in gridAxis.MinorRange) {
+        Assert.Equal(gridAxis.min_value + gridAxis.minor_step * i++, range, 5);
+      }
+    }
+
+    [Fact]
+    public void StepIsWithinExpectedRangeWhenScalingIsLessThanTwoPointFive() {
+      GridAxis gridAxis = new GridAxis(200, -20);
+      Assert.Equal(20, gridAxis.major_step, 5);
+      Assert.Equal(5, gridAxis.minor_step, 5);
+    }
+
+    [Fact]
+    public void StepIsWithinExpectedRangeWhenScalingIsLessThanFive() {
+      GridAxis gridAxis = new GridAxis(450, -20);
+      Assert.Equal(50, gridAxis.major_step, 5);
+      Assert.Equal(10, gridAxis.minor_step, 5);
+    }
+
+    [Fact]
+    public void StepIsWithinExpectedRangeWhenScalingIsLessThanSevenPointFive() {
+      GridAxis gridAxis = new GridAxis(700, -20);
+      Assert.Equal(100, gridAxis.major_step, 5);
+      Assert.Equal(20, gridAxis.minor_step, 5);
+    }
+
+    [Fact]
+    public void StepIsWithinExpectedRangeWhenScalingIsGreaterThanSevenPointFive() {
+      GridAxis gridAxis = new GridAxis(800, -20);
+      Assert.Equal(200, gridAxis.major_step, 5);
+      Assert.Equal(50, gridAxis.minor_step, 5);
+    }
+
+  }
+}
