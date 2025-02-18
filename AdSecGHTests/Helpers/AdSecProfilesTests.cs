f7cbd5e0322abe2403507bb4fcdb619d70306bc7
DominikaLos <dominika.los@arup.com>
2025-02-18 11:57:36 +0100

    refactor: extracted sample profiles into new class

--- AdSecGHTests/Helpers/AdSecProfilesTests.cs
+++ AdSecGHTests/Helpers/AdSecProfilesTests.cs
@@ -18,9 +18,6 @@ using ICircleProfile = Oasys.Profiles.ICircleProfile;
 namespace AdSecGHTests.Helpers {
   [Collection("GrasshopperFixture collection")]
   public class AdSecProfilesTests {
-
-    #region init profiles
-
     private class WebUnknownDummy : IWeb { }
 
     private class ProfileUnknownDummy : IProfile {
@@ -30,272 +27,8 @@ namespace AdSecGHTests.Helpers {
 
     private IAngleProfile angleProfile = null;
 
-    private static AngleProfile SetAngleProfile(IWeb web) {
-      var lengthInMeters = new Length(1, LengthUnit.Meter);
-      var thicknessInMeters = new Length(0.2, LengthUnit.Meter);
-      var depthInInches = new Length(1, LengthUnit.Inch);
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      var flange = new Flange(thicknessInMeters, lengthInMeters);
-
-      return new AngleProfile {
-        Depth = depthInInches,
-        Rotation = rotationInDegrees,
-        Flange = flange,
-        Web = web,
-      };
-    }
-
-    private static CatalogueProfile SetCatalogueProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new CatalogueProfile() {
-        Rotation = rotationInDegrees,
-        Description = "CAT BSI-IPE IPEAA80",
-      };
-    }
-
-    private static ChannelProfile SetChannelProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new ChannelProfile() {
-        Rotation = rotationInDegrees,
-        Web = new WebConstant(new Length(1, LengthUnit.Inch)),
-        Flanges = new Flange(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-        Depth = new Length(3, LengthUnit.Inch),
-      };
-    }
-
-    private static CircleProfile SetCircleProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new CircleProfile() {
-        Rotation = rotationInDegrees,
-        Diameter = new Length(1, LengthUnit.Inch),
-      };
-    }
-
-    private static CircleHollowProfile SetCircleHollowProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new CircleHollowProfile() {
-        WallThickness = new Length(1, LengthUnit.Inch),
-        Rotation = rotationInDegrees,
-        Diameter = new Length(1, LengthUnit.Inch),
-      };
-    }
-
-    private static CruciformSymmetricalProfile SetCruciformSymmetricalProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new CruciformSymmetricalProfile() {
-        Rotation = rotationInDegrees,
-        Web = new WebConstant(new Length(1, LengthUnit.Inch)),
-        Depth = new Length(1, LengthUnit.Inch),
-        Flange = new Flange(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-      };
-    }
-
-    private static EllipseProfile SetEllipseProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new EllipseProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Width = new Length(2, LengthUnit.Inch),
-      };
-    }
-
-    private static EllipseHollowProfile SetEllipseHollowProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new EllipseHollowProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Width = new Length(2, LengthUnit.Inch),
-        WallThickness = new Length(0.2, LengthUnit.Inch),
-      };
-    }
-
-    private static GeneralCProfile SetGeneralCProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new GeneralCProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Thickness = new Length(0.2, LengthUnit.Inch),
-        FlangeWidth = new Length(1, LengthUnit.Inch),
-        Lip = new Length(0.2, LengthUnit.Inch),
-      };
-    }
-
-    private static GeneralZProfile SetGeneralZProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new GeneralZProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Thickness = new Length(0.2, LengthUnit.Inch),
-        BottomFlangeWidth = new Length(1, LengthUnit.Inch),
-        BottomLip = new Length(0.2, LengthUnit.Inch),
-        TopFlangeWidth = new Length(1, LengthUnit.Inch),
-        TopLip = new Length(0.2, LengthUnit.Inch),
-      };
-    }
-
-    private static IBeamProfile SetBeamProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new IBeamProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Web = new WebConstant(new Length(1, LengthUnit.Inch)),
-        Flanges = new Flange(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-      };
-    }
-
-    private static IBeamAsymmetricalProfile SetIBeamAssymetricalProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new IBeamAsymmetricalProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Web = new WebConstant(new Length(1, LengthUnit.Inch)),
-        BottomFlange = new Flange(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-        TopFlange = new Flange(new Length(2, LengthUnit.Inch), new Length(1, LengthUnit.Inch)),
-      };
-    }
-
-    private static IBeamCellularProfile SetBeamCellularProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new IBeamCellularProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Web = new WebConstant(new Length(1, LengthUnit.Inch)),
-        Flanges = new Flange(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-        Spacing = new Length(0.2, LengthUnit.Inch),
-        WebOpening = new Length(0.2, LengthUnit.Inch),
-        OpeningType = IBeamOpeningType.Castellated,
-      };
-    }
-
-    private static PerimeterProfile SetPerimeterProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new PerimeterProfile() {
-        Rotation = rotationInDegrees,
-        Perimeter = new Polygon(new List<IPoint2d>() {
-          new Point2d(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-        }),
-        VoidPolygons = new List<IPolygon>() {
-          new Polygon(new List<IPoint2d>() {
-            new Point2d(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-          }),
-        },
-      };
-    }
-
-    private static RectangleProfile SetRectangleProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new RectangleProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Width = new Length(2, LengthUnit.Inch),
-      };
-    }
-
-    private static RectangleHollowProfile SetRectangleHollowProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new RectangleHollowProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Flanges = new Flange(new Length(1, LengthUnit.Inch), new Length(2, LengthUnit.Inch)),
-        Webs = new WebConstant(new Length(1, LengthUnit.Inch)),
-      };
-    }
-
-    private static RectoCircleProfile SetRectoCircleProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new RectoCircleProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Width = new Length(2, LengthUnit.Inch),
-      };
-    }
-
-    private static RectoEllipseProfile SetRectoEllipseProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new RectoEllipseProfile() {
-        Rotation = rotationInDegrees,
-        Depth = new Length(1, LengthUnit.Inch),
-        Width = new Length(2, LengthUnit.Inch),
-        DepthFlat = new Length(0.2, LengthUnit.Inch),
-        WidthFlat = new Length(0.2, LengthUnit.Inch),
-      };
-    }
-
-    private static SecantPileProfile SetSecantPileProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new SecantPileProfile() {
-        Rotation = rotationInDegrees,
-        Diameter = new Length(1, LengthUnit.Inch),
-        IsWall = true,
-        PileCentres = new Length(0.2, LengthUnit.Inch),
-        PileCount = 2,
-      };
-    }
-
-    private static SheetPileProfile SetSheetPileProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new SheetPileProfile() {
-        Rotation = rotationInDegrees,
-        BottomFlangeWidth = new Length(1, LengthUnit.Inch),
-        FlangeThickness = new Length(0.2, LengthUnit.Inch),
-        Depth = new Length(1, LengthUnit.Inch),
-        TopFlangeWidth = new Length(1, LengthUnit.Inch),
-        WebThickness = new Length(0.2, LengthUnit.Inch),
-        Width = new Length(2, LengthUnit.Inch),
-      };
-    }
-
-    private static TrapezoidProfile SetTrapezoidProfile() {
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      return new TrapezoidProfile() {
-        Rotation = rotationInDegrees,
-        BottomWidth = new Length(1, LengthUnit.Inch),
-        Depth = new Length(1, LengthUnit.Inch),
-        TopWidth = new Length(2, LengthUnit.Inch),
-      };
-    }
-
-    private static TSectionProfile SetTSectionProfile() {
-      var lengthInMeters = new Length(1, LengthUnit.Meter);
-      var thicknessInMeters = new Length(0.2, LengthUnit.Meter);
-      var depthInInches = new Length(1, LengthUnit.Inch);
-      var rotationInDegrees = new Angle(0, AngleUnit.Degree);
-
-      var flange = new Flange(thicknessInMeters, lengthInMeters);
-
-      return new TSectionProfile {
-        Depth = depthInInches,
-        Rotation = rotationInDegrees,
-        Flange = flange,
-        Web = new WebConstant(new Length(1, LengthUnit.Inch)),
-      };
-    }
-
-    #endregion
-
     public AdSecProfilesTests() {
-      angleProfile = SetAngleProfile(new WebConstant(new Length(1, LengthUnit.Inch)));
+      angleProfile = SampleProfiles.GetAnAngleProfile(new WebConstant(new Length(1, LengthUnit.Inch)));
     }
 
     [Fact]
@@ -396,13 +129,12 @@ namespace AdSecGHTests.Helpers {
       var result = AdSecProfiles.CreateProfile(angleProfile);
 
       Assert.NotNull(result);
-      Assert.Equal(2.4, result.SurfaceAreaPerUnitLength().Value);
       Assert.Equal($"{nameof(Oasys.Profiles.IAngleProfile)}_Implementation", result.GetType().Name);
     }
 
     [Fact]
     public void CreateProfile_WhenCatalogueProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetCatalogueProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetACatalogueProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ICatalogueProfile)}_Implementation", result.GetType().Name);
@@ -410,7 +142,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenChannelProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetChannelProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetAChannelProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IChannelProfile)}_Implementation", result.GetType().Name);
@@ -418,7 +150,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenCircleProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetCircleProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetACircleProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ICircleProfile)}_Implementation", result.GetType().Name);
@@ -426,7 +158,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenCircleHollowProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetCircleHollowProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetACircleHollowProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ICircleHollowProfile)}_Implementation", result.GetType().Name);
@@ -434,7 +166,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenCruciformSymmetricalProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetCruciformSymmetricalProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetACruciformSymmetricalProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ICruciformSymmetricalProfile)}_Implementation", result.GetType().Name);
@@ -442,7 +174,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenEllipseProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetEllipseProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetAnEllipseProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IEllipseProfile)}_Implementation", result.GetType().Name);
@@ -450,7 +182,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenEllipseHollowProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetEllipseHollowProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetAnEllipseHollowProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IEllipseHollowProfile)}_Implementation", result.GetType().Name);
@@ -458,7 +190,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenGeneralCProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetGeneralCProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetAGeneralCProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IGeneralCProfile)}_Implementation", result.GetType().Name);
@@ -466,7 +198,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenGeneralZProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetGeneralZProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetAGeneralZProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IGeneralZProfile)}_Implementation", result.GetType().Name);
@@ -474,7 +206,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenBeamProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetBeamProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetABeamProfile());
 
       Assert.NotNull(result);
       Assert.Equal("IIBeamSymmetricalProfile_Implementation", result.GetType().Name);
@@ -482,7 +214,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenIBeamAssymetricalProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetIBeamAssymetricalProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetAnIBeamAssymetricalProfile());
 
       Assert.NotNull(result);
       Assert.Equal("IIBeamAsymmetricalProfile_Implementation", result.GetType().Name);
@@ -490,7 +222,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenIBeamCellularProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetBeamCellularProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetABeamCellularProfile());
 
       Assert.NotNull(result);
       Assert.Equal("IIBeamCellularProfile_Implementation", result.GetType().Name);
@@ -498,7 +230,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenPerimeterProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetPerimeterProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetAPerimeterProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IPerimeterProfile)}_Implementation", result.GetType().Name);
@@ -506,7 +238,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenRectangleProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetRectangleProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetARectangleProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IRectangleProfile)}_Implementation", result.GetType().Name);
@@ -514,7 +246,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenRectangleHollowProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetRectangleHollowProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetARectangleHollowProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IRectangleHollowProfile)}_Implementation", result.GetType().Name);
@@ -522,7 +254,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenRectoCircleProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetRectoCircleProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetARectoCircleProfile());
 
       Assert.NotNull(result);
       Assert.Equal("IStadiumProfile_Implementation", result.GetType().Name);
@@ -530,7 +262,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenRectoEllipseProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetRectoEllipseProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetARectoEllipseProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(IRectoEllipseProfile)}_Implementation", result.GetType().Name);
@@ -538,7 +270,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenSecantPileProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetSecantPileProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetASecantPileProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ISecantPileProfile)}_Implementation", result.GetType().Name);
@@ -546,7 +278,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenSheetPileProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetSheetPileProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetASheetPileProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ISheetPileProfile)}_Implementation", result.GetType().Name);
@@ -554,7 +286,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenTrapezoidProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetTrapezoidProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetATrapezoidProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ITrapezoidProfile)}_Implementation", result.GetType().Name);
@@ -562,7 +294,7 @@ namespace AdSecGHTests.Helpers {
 
     [Fact]
     public void CreateProfile_WhenTSectionProfileType_ReturnValidProfile() {
-      var result = AdSecProfiles.CreateProfile(SetTSectionProfile());
+      var result = AdSecProfiles.CreateProfile(SampleProfiles.GetATSectionProfile());
 
       Assert.NotNull(result);
       Assert.Equal($"{nameof(ITSectionProfile)}_Implementation", result.GetType().Name);
---
+++ AdSecGHTests/Helpers/SampleProfiles.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections.Generic;
+
+using Oasys.Taxonomy.Geometry;
+using Oasys.Taxonomy.Profiles;
+
+using OasysGH.Units;
+
+using OasysUnits;
+using OasysUnits.Units;
+
+namespace AdSecGHTests.Helpers {
+  public static class SampleProfiles {
+    private static Length GetThickness() {
+      return new Length(0.2, LengthUnit.Meter);
+    }
+
+    private static Flange GetFlange() {
+      return new Flange(GetThickness(), LengthOne());
+    }
+
+    private static Length LengthOne() {
+      return new Length(1, DefaultUnits.LengthUnitGeometry);
+    }
+
+    private static WebConstant GetWebConstant() {
+      return new WebConstant(GetThickness());
+    }
+
+    public static AngleProfile GetAnAngleProfile(IWeb web) {
+      return new AngleProfile {
+        Depth = LengthOne(),
+        Rotation = Angle.Zero,
+        Flange = GetFlange(),
+        Web = web,
+      };
+    }
+
+    public static CatalogueProfile GetACatalogueProfile() {
+      return new CatalogueProfile() {
+        Rotation = Angle.Zero,
+        Description = "CAT BSI-IPE IPEAA80",
+      };
+    }
+
+    public static ChannelProfile GetAChannelProfile() {
+      return new ChannelProfile() {
+        Rotation = Angle.Zero,
+        Web = GetWebConstant(),
+        Flanges = GetFlange(),
+        Depth = LengthOne(),
+      };
+    }
+
+    public static CircleProfile GetACircleProfile() {
+      return new CircleProfile() {
+        Rotation = Angle.Zero,
+        Diameter = LengthOne(),
+      };
+    }
+
+    public static CircleHollowProfile GetACircleHollowProfile() {
+      return new CircleHollowProfile() {
+        WallThickness = GetThickness(),
+        Rotation = Angle.Zero,
+        Diameter = LengthOne(),
+      };
+    }
+
+    public static CruciformSymmetricalProfile GetACruciformSymmetricalProfile() {
+      return new CruciformSymmetricalProfile() {
+        Rotation = Angle.Zero,
+        Web = GetWebConstant(),
+        Depth = LengthOne(),
+        Flange = GetFlange(),
+      };
+    }
+
+    public static EllipseProfile GetAnEllipseProfile() {
+      return new EllipseProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Width = LengthOne(),
+      };
+    }
+
+    public static EllipseHollowProfile GetAnEllipseHollowProfile() {
+      return new EllipseHollowProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Width = LengthOne(),
+        WallThickness = GetThickness(),
+      };
+    }
+
+    public static GeneralCProfile GetAGeneralCProfile() {
+      return new GeneralCProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Thickness = GetThickness(),
+        FlangeWidth = LengthOne(),
+        Lip = LengthOne(),
+      };
+    }
+
+    public static GeneralZProfile GetAGeneralZProfile() {
+      return new GeneralZProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Thickness = GetThickness(),
+        BottomFlangeWidth = LengthOne(),
+        BottomLip = GetThickness(),
+        TopFlangeWidth = LengthOne(),
+        TopLip = GetThickness(),
+      };
+    }
+
+    public static IBeamProfile GetABeamProfile() {
+      return new IBeamProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Web = GetWebConstant(),
+        Flanges = GetFlange(),
+      };
+    }
+
+    public static IBeamAsymmetricalProfile GetAnIBeamAssymetricalProfile() {
+      return new IBeamAsymmetricalProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Web = GetWebConstant(),
+        BottomFlange = GetFlange(),
+        TopFlange = GetFlange(),
+      };
+    }
+
+    public static IBeamCellularProfile GetABeamCellularProfile() {
+      return new IBeamCellularProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Web = GetWebConstant(),
+        Flanges = GetFlange(),
+        Spacing = GetThickness(),
+        WebOpening = GetThickness(),
+        OpeningType = IBeamOpeningType.Castellated,
+      };
+    }
+
+    public static PerimeterProfile GetAPerimeterProfile() {
+      return new PerimeterProfile() {
+        Rotation = Angle.Zero,
+        Perimeter = new Polygon(new List<IPoint2d>() {
+          new Point2d(LengthOne(), LengthOne()),
+        }),
+        VoidPolygons = new List<IPolygon>() {
+          new Polygon(new List<IPoint2d>() {
+            new Point2d(LengthOne(), LengthOne()),
+          }),
+        },
+      };
+    }
+
+    public static RectangleProfile GetARectangleProfile() {
+      return new RectangleProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Width = LengthOne(),
+      };
+    }
+
+    public static RectangleHollowProfile GetARectangleHollowProfile() {
+      return new RectangleHollowProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Flanges = GetFlange(),
+        Webs = GetWebConstant(),
+      };
+    }
+
+    public static RectoCircleProfile GetARectoCircleProfile() {
+      return new RectoCircleProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Width = LengthOne(),
+      };
+    }
+
+    public static RectoEllipseProfile GetARectoEllipseProfile() {
+      return new RectoEllipseProfile() {
+        Rotation = Angle.Zero,
+        Depth = LengthOne(),
+        Width = LengthOne(),
+        DepthFlat = GetThickness(),
+        WidthFlat = GetThickness(),
+      };
+    }
+
+    public static SecantPileProfile GetASecantPileProfile() {
+      return new SecantPileProfile() {
+        Rotation = Angle.Zero,
+        Diameter = LengthOne(),
+        IsWall = true,
+        PileCentres = GetThickness(),
+        PileCount = 2,
+      };
+    }
+
+    public static SheetPileProfile GetASheetPileProfile() {
+      return new SheetPileProfile() {
+        Rotation = Angle.Zero,
+        BottomFlangeWidth = LengthOne(),
+        FlangeThickness = GetThickness(),
+        Depth = LengthOne(),
+        TopFlangeWidth = LengthOne(),
+        WebThickness = GetThickness(),
+        Width = LengthOne(),
+      };
+    }
+
+    public static TrapezoidProfile GetATrapezoidProfile() {
+      return new TrapezoidProfile() {
+        Rotation = Angle.Zero,
+        BottomWidth = LengthOne(),
+        Depth = LengthOne(),
+        TopWidth = LengthOne(),
+      };
+    }
+
+    public static TSectionProfile GetATSectionProfile() {
+      return new TSectionProfile {
+        Depth = LengthOne(),
+        Rotation = Angle.Zero,
+        Flange = GetFlange(),
+        Web = GetWebConstant(),
+      };
+    }
+  }
+}
