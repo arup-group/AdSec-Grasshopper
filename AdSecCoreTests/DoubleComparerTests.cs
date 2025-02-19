eb83ad2a6aa2a8a26e93e4e4c03186d5ea5da3f2
spsarras <psarras.st@gmail.com>
2025-02-19 15:36:16 +0000

    test: double comparer leftover

--- AdSecCoreTests/DoubleComparerTests.cs
+++ AdSecCoreTests/DoubleComparerTests.cs
@@ -3,13 +3,17 @@ using AdSecCore;
 namespace AdSecCoreTests {
   public class DoubleComparerTests {
     [Fact]
-    public void ValuesDifferLessThanEpsilonShouldBeConsideredEqual() {
+    public void ValuesDifferLessThanEpsilonShouldBeConsideredEqualNoMargin() {
       Assert.Equal(10.0, 10.01, new DoubleComparer());
     }
 
     [Fact]
+    public void ValuesDifferLessThanEpsilonShouldBeConsideredEqualWithMargin() {
+      Assert.Equal(10.0, 10.001, new DoubleComparer(0.1, true));
+    }
+    [Fact]
     public void ValuesLargerThanEpsilonShouldBeConsideredEqual() {
-      int epsilon = 1;
+      double epsilon = 1f;
       Assert.NotEqual(10.0, 10.0 + (epsilon * 2), new DoubleComparer(epsilon, true));
     }
   }
