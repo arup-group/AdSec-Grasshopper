81923ead5891b4d60269337ea5a5846a6a13ffc4
Stamatios Psarras <psarras.st@gmail.com>
2025-02-25 10:41:28 +0000

    ADSECGH-75: test coverage create point (#124)
    * feat: inputs are getting updated from Dictionary
    
    * test: CreatePoint should start hidden
    
    * refactor: changed PointRebar to the Adapter
    
    * test: CreatePoint Function
    
    * refactor: Added all inputs for point rebar
    
    * fix: units need to be part of the attribute names
    
    * test: increased coverage
    
    * feat: changing units twice renames property correctly
    
    * refactor: removed unused variable in tests

---
+++ AdSecCoreTests/Functions/PointRebarTests.cs
@@ -0,0 +1,47 @@
+﻿using AdSecCore.Functions;
+
+using AdSecGHCore.Constants;
+
+using OasysUnits;
+using OasysUnits.Units;
+
+namespace AdSecCoreTests.Functions {
+  public class PointRebarTests {
+    private readonly PointRebarFunction pointRebarFunction;
+
+    public PointRebarTests() {
+      pointRebarFunction = new PointRebarFunction();
+      pointRebarFunction.Y.Value = Length.From(1, LengthUnit.Meter);
+      pointRebarFunction.Z.Value = Length.From(1, LengthUnit.Meter);
+      pointRebarFunction.Compute();
+    }
+
+    [Fact]
+    public void ShouldReturnOneOutput() {
+      Assert.Single(pointRebarFunction.GetAllOutputAttributes());
+    }
+
+    [Fact]
+    public void ShouldComputeValidPoint() {
+      var point = pointRebarFunction.Point.Value;
+      Assert.Equal(1, point.Y.Value);
+      Assert.Equal(1, point.Z.Value);
+      Assert.Single(pointRebarFunction.GetAllOutputAttributes());
+    }
+
+    [Fact]
+    public void ShouldStartHidden() {
+      Assert.False(pointRebarFunction.Organisation.Hidden);
+    }
+
+    [Fact]
+    public void ShouldBeOnSubCat3() {
+      Assert.Equal(SubCategoryName.Cat3(), pointRebarFunction.Organisation.SubCategory);
+    }
+
+    [Fact]
+    public void ShouldBeOnCatName() {
+      Assert.Equal(CategoryName.Name(), pointRebarFunction.Organisation.Category);
+    }
+  }
+}
