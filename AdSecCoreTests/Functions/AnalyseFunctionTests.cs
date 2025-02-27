56b54181a3672fbc207ac2c7b6a1020af1a28715
spsarras <psarras.st@gmail.com>
2025-02-27 16:02:01 +0000

    test(AnalyseFunctionTests): Added Stabs for Output Attributes and Compute

---
+++ AdSecCoreTests/Functions/AnalyseFunctionTests.cs
@@ -0,0 +1,25 @@
+﻿using AdSecCore.Functions;
+
+namespace AdSecCoreTests.Functions {
+  public class AnalyseFunctionTests {
+
+    private readonly AnalyseFunction analyseFunction;
+
+    public AnalyseFunctionTests() {
+      analyseFunction = new AnalyseFunction();
+    }
+
+    [Fact]
+    public void ShouldHaveOneInput() {
+      Assert.Single(analyseFunction.GetAllInputAttributes());
+    }
+
+    [Fact]
+    public void ShouldHaveTwoOutputs() {
+      Assert.Equal(2, analyseFunction.GetAllOutputAttributes().Length);
+    }
+
+    [Fact(Skip = "Not implemented yet")]
+    public void ShouldComputOutputs() { }
+  }
+}
