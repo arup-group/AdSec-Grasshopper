1894447002eba9b23bf5b075f7712ce96ebfd2ae
spsarras <psarras.st@gmail.com>
2025-03-03 11:05:44 +0000

    feat(AdSecSolutionGoo): removed checks for null as they are not needed

--- AdSecGH/Parameters/AdSecSolutionGoo.cs
+++ AdSecGH/Parameters/AdSecSolutionGoo.cs
@@ -31,8 +31,8 @@ namespace AdSecGH.Parameters {
 
     public AdSecSolutionGoo(ISolution solution, AdSecSection section) : base(solution) {
       m_section = section;
-      m_plane = m_section == null ? Plane.WorldXY : m_section.LocalPlane;
-      ProfileEdge = m_section?.m_profileEdge;
+      m_plane = m_section.LocalPlane;
+      ProfileEdge = m_section.m_profileEdge;
     }
 
     public override IGH_Goo Duplicate() {
