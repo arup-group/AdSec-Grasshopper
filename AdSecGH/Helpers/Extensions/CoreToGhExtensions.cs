5ab8ff1d91825c282602626adb341ad8943ebc67
spsarras <psarras.st@gmail.com>
2025-03-13 13:33:47 +0000

    feat(CoreToGhExtensions): Added Convertors for Plane to OasysPlane

--- AdSecGH/Helpers/Extensions/CoreToGhExtensions.cs
+++ AdSecGH/Helpers/Extensions/CoreToGhExtensions.cs
@@ -12,12 +12,28 @@ namespace AdSecGH.Helpers {
       return new Plane(origin, x, y);
     }
 
+    public static OasysPlane ToOasys(this Plane plane) {
+      return new OasysPlane() {
+        Origin = plane.Origin.ToOasysPoint(),
+        XAxis = plane.XAxis.ToOasysPoint(),
+        YAxis = plane.YAxis.ToOasysPoint()
+      };
+    }
+
     public static Point3d ToGhPoint(this OasysPoint point) {
       return new Point3d(point.X, point.Y, point.Z);
     }
 
+    public static OasysPoint ToOasysPoint(this Point3d point) {
+      return new OasysPoint() { X = point.X, Y = point.Y, Z = point.Z };
+    }
+
     public static Vector3d ToGhVector(this OasysPoint point) {
       return new Vector3d(point.X, point.Y, point.Z);
+    }
+
+    public static OasysPoint ToOasysPoint(this Vector3d vector) {
+      return new OasysPoint() { X = vector.X, Y = vector.Y, Z = vector.Z };
     }
 
   }
