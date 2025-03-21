2075591dd36423c53a47ac4cffc45bb1fc4ad2a1
spsarras <psarras.st@gmail.com>
2025-03-21 10:57:52 +0000

    refactor(AdSecSectionGoo): simplified the selected vs deselected visualisation of curves

--- AdSecGH/Parameters/AdSecSectionGoo.cs
+++ AdSecGH/Parameters/AdSecSectionGoo.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
 
+using Rhino;
+using Rhino.DocObjects;
 using Rhino.Geometry;
 
 namespace AdSecGH.Parameters {
@@ -13,9 +16,11 @@ namespace AdSecGH.Parameters {
         if (Value == null) {
           return BoundingBox.Empty;
         }
+
         if (Value.SolidBrep == null) {
           return BoundingBox.Empty;
         }
+
         return Value.SolidBrep.GetBoundingBox(false);
       }
     }
@@ -25,9 +30,11 @@ namespace AdSecGH.Parameters {
         if (Value == null) {
           return false;
         }
+
         if (Value.SolidBrep == null || Value.IsValid == false) {
           return false;
         }
+
         return true;
       }
     }
@@ -36,14 +43,14 @@ namespace AdSecGH.Parameters {
         if (Value.IsValid) {
           return string.Empty;
         }
+
         return Value.IsValid.ToString();
       }
     }
     public override string TypeDescription => $"AdSec {TypeName} Parameter";
     public override string TypeName => "Section";
 
-    public AdSecSectionGoo() {
-    }
+    public AdSecSectionGoo() { }
 
     public AdSecSectionGoo(AdSecSection section) {
       if (section == null) {
@@ -82,38 +89,48 @@ namespace AdSecGH.Parameters {
         } else {
           target = (Q)(object)Value.Duplicate();
         }
+
         return true;
       }
+
       if (typeof(Q).IsAssignableFrom(typeof(AdSecSection))) {
         if (Value == null) {
           target = default;
         } else {
-          target = (Q)(object)new AdSecSection(Value.Section, Value.DesignCode, Value._codeName, Value._materialName, Value.LocalPlane);
+          target = (Q)(object)new AdSecSection(Value.Section, Value.DesignCode, Value._codeName, Value._materialName,
+            Value.LocalPlane);
         }
+
         return true;
       }
+
       if (typeof(Q).IsAssignableFrom(typeof(AdSecProfileGoo))) {
         if (Value == null) {
           target = default;
         } else {
           target = (Q)(object)new AdSecProfileGoo(Value.Section.Profile, Value.LocalPlane);
         }
+
         return true;
       }
+
       if (typeof(Q).IsAssignableFrom(typeof(Brep))) {
         if (Value == null) {
           target = default;
         } else {
           target = (Q)(object)Value.SolidBrep.DuplicateBrep();
         }
+
         return true;
       }
+
       if (typeof(Q).IsAssignableFrom(typeof(GH_Brep))) {
         if (Value == null) {
           target = default;
         } else {
           target = (Q)(object)Value.SolidBrep.DuplicateBrep();
         }
+
         return true;
       }
 
@@ -130,6 +147,7 @@ namespace AdSecGH.Parameters {
         for (int i = 0; i < Value._subProfiles.Count; i++) {
           args.Pipeline.DrawBrepShaded(Value._subProfiles[i], Value.m_subColours[i]);
         }
+
         // draw rebars
         for (int i = 0; i < Value.m_rebars.Count; i++) {
           args.Pipeline.DrawBrepShaded(Value.m_rebars[i], Value.m_rebarColours[i]);
@@ -138,76 +156,57 @@ namespace AdSecGH.Parameters {
     }
 
     public void DrawViewportWires(GH_PreviewWireArgs args) {
-      if (Value == null) { return; }
+      if (Value == null) {
+        return;
+      }
 
-      Color defaultCol = Grasshopper.Instances.Settings.GetValue("DefaultPreviewColour", Color.White);
-      if (args.Color.R == defaultCol.R && args.Color.G == defaultCol.G && args.Color.B == defaultCol.B) // not selected
-      {
-        args.Pipeline.DrawPolyline(Value.m_profileEdge, UI.Colour.OasysBlue, 2);
-        if (Value.m_profileVoidEdges != null) {
-          foreach (Polyline crv in Value.m_profileVoidEdges) {
-            args.Pipeline.DrawPolyline(crv, UI.Colour.OasysBlue, 1);
-          }
+      var primaryColor = IsSelected(args) ? UI.Colour.OasysBlue : UI.Colour.OasysYellow;
+      var secondaryColor = IsSelected(args) ? Color.Black : UI.Colour.UILightGrey;
+      var primaryThickness = IsSelected(args) ? 2 : 3;
+      var secondaryThickness = IsSelected(args) ? 2 : 1;
+
+      args.Pipeline.DrawPolyline(Value.m_profileEdge, primaryColor, primaryThickness);
+      if (Value.m_profileVoidEdges != null) {
+        foreach (Polyline crv in Value.m_profileVoidEdges) {
+          args.Pipeline.DrawPolyline(crv, primaryColor, secondaryThickness);
         }
-        if (Value.m_subEdges != null) {
-          foreach (Polyline crv in Value.m_subEdges) {
-            args.Pipeline.DrawPolyline(crv, UI.Colour.OasysBlue, 1);
-          }
+      }
+
+      if (Value.m_subEdges != null) {
+        foreach (Polyline crv in Value.m_subEdges) {
+          args.Pipeline.DrawPolyline(crv, primaryColor, secondaryThickness);
         }
-        if (Value.m_subVoidEdges != null) {
-          foreach (List<Polyline> crvs in Value.m_subVoidEdges) {
-            foreach (Polyline crv in crvs) {
-              args.Pipeline.DrawPolyline(crv, UI.Colour.OasysBlue, 1);
-            }
-          }
-        }
-        if (Value.m_rebarEdges != null) {
-          foreach (Circle crv in Value.m_rebarEdges) {
-            args.Pipeline.DrawCircle(crv, Color.Black, 1);
-          }
-        }
-        if (Value.m_linkEdges != null) {
-          foreach (Curve crv in Value.m_linkEdges) {
-            args.Pipeline.DrawCurve(crv, Color.Black, 1);
-          }
-        }
-      } else // selected
-        {
-        args.Pipeline.DrawPolyline(Value.m_profileEdge, UI.Colour.OasysYellow, 3);
-        if (Value.m_profileVoidEdges != null) {
-          foreach (Polyline crv in Value.m_profileVoidEdges) {
-            args.Pipeline.DrawPolyline(crv, UI.Colour.OasysYellow, 2);
-          }
-        }
-        if (Value.m_subEdges != null) {
-          foreach (Polyline crv in Value.m_subEdges) {
-            args.Pipeline.DrawPolyline(crv, UI.Colour.OasysYellow, 2);
-          }
-        }
-        if (Value.m_subVoidEdges != null) {
-          foreach (List<Polyline> crvs in Value.m_subVoidEdges) {
-            foreach (Polyline crv in crvs) {
-              args.Pipeline.DrawPolyline(crv, UI.Colour.OasysYellow, 2);
-            }
-          }
-        }
-        if (Value.m_rebarEdges != null) {
-          foreach (Circle crv in Value.m_rebarEdges) {
-            args.Pipeline.DrawCircle(crv, UI.Colour.UILightGrey, 2);
-          }
-        }
-        if (Value.m_linkEdges != null) {
-          foreach (Curve crv in Value.m_linkEdges) {
-            args.Pipeline.DrawCurve(crv, UI.Colour.UILightGrey, 2);
+      }
+
+      if (Value.m_subVoidEdges != null) {
+        foreach (List<Polyline> crvs in Value.m_subVoidEdges) {
+          foreach (Polyline crv in crvs) {
+            args.Pipeline.DrawPolyline(crv, primaryColor, secondaryThickness);
           }
         }
       }
+
+      if (Value.m_rebarEdges != null) {
+        foreach (Circle crv in Value.m_rebarEdges) {
+          args.Pipeline.DrawCircle(crv, secondaryColor, secondaryThickness);
+        }
+      }
+
+      if (Value.m_linkEdges != null) {
+        foreach (Curve crv in Value.m_linkEdges) {
+          args.Pipeline.DrawCurve(crv, secondaryColor, secondaryThickness);
+        }
+      }
+
       // local axis
-      if (Value.previewXaxis != null) {
-        args.Pipeline.DrawLine(Value.previewZaxis, Color.FromArgb(255, 244, 96, 96), 1);
-        args.Pipeline.DrawLine(Value.previewXaxis, Color.FromArgb(255, 96, 244, 96), 1);
-        args.Pipeline.DrawLine(Value.previewYaxis, Color.FromArgb(255, 96, 96, 234), 1);
-      }
+      args.Pipeline.DrawLine(Value.previewZaxis, Color.FromArgb(255, 244, 96, 96), 1);
+      args.Pipeline.DrawLine(Value.previewXaxis, Color.FromArgb(255, 96, 244, 96), 1);
+      args.Pipeline.DrawLine(Value.previewYaxis, Color.FromArgb(255, 96, 96, 234), 1);
+    }
+
+    private static bool IsSelected(GH_PreviewWireArgs args) {
+      Color defaultCol = Grasshopper.Instances.Settings.GetValue("DefaultPreviewColour", Color.White);
+      return args.Color.R == defaultCol.R && args.Color.G == defaultCol.G && args.Color.B == defaultCol.B;
     }
 
     public AdSecSectionGoo DuplicateAdSecSection() {
@@ -226,9 +225,11 @@ namespace AdSecGH.Parameters {
       if (Value == null) {
         return BoundingBox.Empty;
       }
+
       if (Value.SolidBrep == null) {
         return BoundingBox.Empty;
       }
+
       return Value.SolidBrep.GetBoundingBox(xform);
     }
 
