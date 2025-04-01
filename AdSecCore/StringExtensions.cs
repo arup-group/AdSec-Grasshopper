b4ce0225e425d1842275cfc66e4ce8e0c3b6bd91
DominikaLos <dominika.los@arup.com>
2025-04-01 14:37:16 +0200

    refactor: added time limit for regex

--- AdSecCore/StringExtensions.cs
+++ AdSecCore/StringExtensions.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace AdSecCore {
   public static class StringExtensions {
     public static string ReplaceNonAlphanumeric(this string input) {
-      return Regex.Replace(input, "[0-9]", string.Empty).Replace(".", string.Empty).Replace("-", string.Empty);
+      return Regex.Replace(input, "[0-9]", string.Empty, RegexOptions.None, TimeSpan.FromSeconds(5))
+       .Replace(".", string.Empty).Replace("-", string.Empty);
     }
   }
 }
