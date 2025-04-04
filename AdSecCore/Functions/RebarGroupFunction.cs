d417ae5b0160ade0c6101283d7b3191bd2e4b0c0
spsarras <psarras.st@gmail.com>
2025-04-04 12:03:22 +0100

    feat(RebarGroupFunction): new function basic organisation and metadata

---
+++ AdSecCore/Functions/RebarGroupFunction.cs
@@ -0,0 +1,18 @@
+﻿using AdSecGHCore.Constants;
+
+namespace AdSecCore.Functions {
+  public class RebarGroupFunction : Function {
+
+    public override FuncAttribute Metadata { get; set; } = new FuncAttribute() {
+      Name = "Create Reinforcement Group",
+      NickName = "Reinforcement Group",
+      Description = "Create a Template Reinforcement Group for an AdSec Section"
+    };
+
+    public override Organisation Organisation { get; set; } = new Organisation() {
+      Category = CategoryName.Name(),
+      SubCategory = SubCategoryName.Cat3()
+    };
+    public override void Compute() { throw new System.NotImplementedException(); }
+  }
+}
---
+++ AdSecCoreTests/Functions/RebarGroupFunctionTests.cs
@@ -0,0 +1,29 @@
+﻿using AdSecCore.Functions;
+
+using AdSecGHCore.Constants;
+
+namespace AdSecCoreTests.Functions {
+  public class RebarGroupFunctionTests {
+
+    private readonly RebarGroupFunction function;
+
+    public RebarGroupFunctionTests() {
+      function = new RebarGroupFunction();
+    }
+
+    [Fact]
+    public void ShouldHaveMetadata() {
+      Assert.NotNull(function.Metadata.Name);
+      Assert.Equal("Create Reinforcement Group", function.Metadata.Name);
+      Assert.Equal("Reinforcement Group", function.Metadata.NickName);
+    }
+
+    [Fact]
+    public void ShouldHaveOrganisation() {
+      Assert.NotNull(function.Organisation);
+      Assert.Equal(CategoryName.Name(), function.Organisation.Category);
+      Assert.Equal(SubCategoryName.Cat3(), function.Organisation.SubCategory);
+    }
+
+  }
+}
