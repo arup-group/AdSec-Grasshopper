b312e45f543420afbf3daf9b0e9ec95e758d88b4
DominikaLos <dominika.los@arup.com>
2025-04-09 08:59:52 +0200

    refactor: added supression messages for locking oasysMenu object

--- AdSecGH/UI/LoadMainMenu.cs
+++ AdSecGH/UI/LoadMainMenu.cs
@@ -29,11 +29,15 @@ namespace AdSecGH.Graphics.Menu {
       if (!editor.MainMenuStrip.Items.ContainsKey("Oasys")) {
         editor.MainMenuStrip.Items.Add(oasysMenu);
       } else {
+#pragma warning disable S2445 // Blocks should be synchronized on read-only fields
+#pragma warning disable S3998 // Threads should not lock on objects with weak identity
         oasysMenu = (ToolStripMenuItem)editor.MainMenuStrip.Items["Oasys"];
         lock (oasysMenu) {
           oasysMenu.DropDown.Items.Add(new ToolStripSeparator());
           PopulateSub(oasysMenu);
         }
+#pragma warning restore S3998 // Threads should not lock on objects with weak identity
+#pragma warning restore S2445 // Blocks should be synchronized on read-only fields
       }
 
       Instances.CanvasCreated -= OnStartup;
