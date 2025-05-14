b3ee652583fe3d76d27849d930519ffd4190bd6a
spsarras <psarras.st@gmail.com>
2025-05-14 18:02:57 +0100

    test(CreateRebarTests): extra test for units

--- AdSecGHTests/Components/3_Rebar/CreateRebarTests.cs
+++ AdSecGHTests/Components/3_Rebar/CreateRebarTests.cs
@@ -1,8 +1,19 @@
-﻿using AdSecGH;
+﻿using AdSecCore.Functions;
+
+using AdSecGH;
 using AdSecGH.Components;
+using AdSecGH.Parameters;
 using AdSecGH.Properties;
 
+using AdSecGHTests.Helpers;
+
+using Oasys.AdSec.DesignCode;
+using Oasys.AdSec.StandardMaterials;
 using Oasys.GH.Helpers;
+
+using OasysGH.Units;
+
+using OasysUnits.Units;
 
 using Xunit;
 
@@ -39,9 +50,29 @@ namespace AdSecGHTests.Components._3_Rebar {
     }
 
     [Fact]
+    public void ShouldMaintainUnitSelectionAfterCompute() {
+      component.SetSelected(0, 0);
+      component.SetSelected(1, 0);
+      var materialDesign = new MaterialDesign() {
+        Material = Reinforcement.Steel.IS456.Edition_2000.S250,
+        DesignCode = new DesignCode() {
+          IDesignCode = IS456.Edition_2000,
+        }
+      };
+      var adSecMaterial = new AdSecMaterialGoo(materialDesign);
+      component.SetInputParamAt(0, adSecMaterial);
+      component.SetInputParamAt(1, 10);
+      ComponentTestHelper.ComputeData(component);
+      var output = component.GetOutputParamAt(0);
+      Assert.NotNull(output);
+      Assert.Equal("Diameter [mm]", component.Params.Input[1].Name);
+    }
+
+    [Fact]
     public void ShouldHaveTwoDropDowns() {
       Assert.Equal(2, component.DropDownItems.Count);
     }
+
 
     [Fact]
     public void ShouldHavePluginInfoReferenced() {
