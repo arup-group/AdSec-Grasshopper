8d6b11a83e611660077832718330340437d0ae40
Stamatios Psarras <psarras.st@gmail.com>
2025-05-23 12:49:48 +0100

    ADSECGH-143: Tests for Create Rebar (#183)
    * refactor(CreateRebarFunction): added organisation and metadata
    
    * refactor(CreateRebarFunction): added output and first layer of inputs
    
    * test(CreateRebarTests): tests for parameters
    
    * feat(createRebar): handle Bundle Inputs
    
    * test(CreateRebarTests): test for number of attributes
    
    * test(CreateRebarTests): added a few tests for dropdowns
    
    * test(CreateRebarTests): test options
    
    * test(CreateRebarTests): test set mode
    
    * test(CreateRebarTests): test LengthUnit and also check it is displayed at the start on Diameter
    
    * fix(CreateRebarTests): fixed issues with tests relating to units
    
    * fix(IFunction.cs): constructor should be protected as it is abstract class
    
    * fix(IFunction.cs): removed constructor and add it on derived classes instead
    
    * fix: UpdatedParameter is sealed since we are calling it on the constructor
    
    * refactor(CreateRebar): remove local versions of FoldMode, Units
    
    * fix(CreateRebar): units update beforeSolveInstance
    
    * test(CreateRebarTests): test for icon and instance
    
    * test(CreateRebarFunction): added single bar function
    
    * refactor(CreateRebarFunction): added function for bundle
    
    * test(CreateRebarFunction): simple test for diameter
    
    * test(CreateRebarFunction): test for Bundle and remove previous implementation
    
    * refactor(CreateRebar): Remove dead code
    
    * test(CreateRebarTests): extra test for units
    
    * refactor(CreateRebar): refactoring the way we pass the local unit
    
    * refactor(CreateRebar): commenting ond removed commented out code
    
    * fix(CreateRebarFunction): override Update unit function
    
    * Fix(CreateRebarFunction): renamed method on compute
    
    * Fix(CreateRebar): reference to local units method
    
    * feat(createRebar): encapsulate field
    
    * test(CreateRebarFunctionTests): directly test UpdateUnits
    
    * test(CreateRebarFunctionTests): test invalid modes
    
    * test(createRebarFunction): coverage for Function through a new interface
    
    * test(createRebarFunction): simplified and added tests cover updating the units
    
    * fix(CreateRebar): remove unecessary call to before Solve Instance
    
    * refactor(CreateRebar,DropdownAdapter): refactor code to correctly update unit
    
    * test(CreateRebarTests): check unit after compute
    
    ---------
    
    Co-authored-by: SandeepArup <61278421+SandeepArup@users.noreply.github.com>
    Co-authored-by: Sandeep Kumar <sandeep.kumar@arup.com>

--- AdSecGH/Components/2_Rebar/CreateRebar.cs
+++ AdSecGH/Components/2_Rebar/CreateRebar.cs
@@ -1,40 +1,23 @@
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 
-using AdSecGH.Helpers;
-using AdSecGH.Parameters;
+using AdSecCore.Functions;
+
 using AdSecGH.Properties;
 
-using AdSecGHCore.Constants;
-
 using Grasshopper.Kernel;
-using Grasshopper.Kernel.Parameters;
 
-using Oasys.AdSec.Materials;
-using Oasys.AdSec.Reinforcement;
+using Oasys.GH.Helpers;
 
 using OasysGH;
-using OasysGH.Components;
-using OasysGH.Helpers;
-using OasysGH.Units;
 using OasysGH.Units.Helpers;
 
-using OasysUnits;
 using OasysUnits.Units;
 
+using CreateRebarFunction = AdSecCore.Functions.CreateRebarFunction;
+
 namespace AdSecGH.Components {
-  public class CreateRebar : GH_OasysDropDownComponent {
-    private LengthUnit _lengthUnit = DefaultUnits.LengthUnitGeometry;
-    private FoldMode _mode = FoldMode.Single;
-
-    public CreateRebar() : base("Create Rebar", "Rebar", "Create Rebar (single or bundle) for an AdSec Section",
-      CategoryName.Name(), SubCategoryName.Cat3()) {
-      Hidden = false; // sets the initial state of the component to hidden
-    }
-
-    // This region handles how the component in displayed on the ribbon including name, exposure level and icon
+  public class CreateRebar : DropdownAdapter<CreateRebarFunction> {
     public override Guid ComponentGuid => new Guid("024d241a-b6cc-4134-9f5c-ac9a6dcb2c4b");
     public override GH_Exposure Exposure => GH_Exposure.primary;
     public override OasysPluginInfo PluginInfo => AdSecGH.PluginInfo.Instance;
@@ -43,109 +26,20 @@ namespace AdSecGH.Components {
     public override void SetSelected(int i, int j) {
       _selectedItems[i] = _dropDownItems[i][j];
       if (i == 0) {
-        _mode = (FoldMode)Enum.Parse(typeof(FoldMode), _selectedItems[i]);
-        ToggleInput();
+        BusinessComponent.SetMode(UpdateMode());
       } else {
-        _lengthUnit = (LengthUnit)UnitsHelper.Parse(typeof(LengthUnit), _selectedItems[i]);
+        SetLocalUnits();
       }
     }
 
-    public override void VariableParameterMaintenance() {
-      string unitAbbreviation = Length.GetAbbreviation(_lengthUnit);
-      Params.Input[1].Name = $"Diameter [{unitAbbreviation}]";
-      if (_mode == FoldMode.Bundle) {
-        Params.Input[2].Name = "Count";
-        Params.Input[2].NickName = "N";
-        Params.Input[2].Description = "Count per bundle (1, 2, 3 or 4)";
-        Params.Input[2].Access = GH_ParamAccess.item;
-        Params.Input[2].Optional = false;
-      }
+
+    internal override void SetLocalUnits() {
+      BusinessComponent.LocalLengthUnitGeometry = (LengthUnit)UnitsHelper.Parse(typeof(LengthUnit), _selectedItems[1]);
+      BusinessComponent.UpdateUnits();
     }
 
-    protected override void InitialiseDropdowns() {
-      _spacerDescriptions = new List<string>(new[] {
-        "Rebar Type",
-        "Measure",
-      });
-
-      _dropDownItems = new List<List<string>>();
-      _selectedItems = new List<string>();
-
-      _dropDownItems.Add(Enum.GetNames(typeof(FoldMode)).ToList());
-      _selectedItems.Add(_dropDownItems[0][0]);
-
-      _dropDownItems.Add(UnitsHelper.GetFilteredAbbreviations(EngineeringUnits.Length));
-      _selectedItems.Add(Length.GetAbbreviation(_lengthUnit));
-
-      _isInitialised = true;
-    }
-
-    protected override void RegisterInputParams(GH_InputParamManager pManager) {
-      string unitAbbreviation = Length.GetAbbreviation(_lengthUnit);
-      pManager.AddGenericParameter("Material", "Mat", "AdSec Reinforcement Material", GH_ParamAccess.item);
-      pManager.AddGenericParameter($"Diameter [{unitAbbreviation}]", "Ø", "Bar Diameter", GH_ParamAccess.item);
-      _mode = FoldMode.Single;
-    }
-
-    protected override void RegisterOutputParams(GH_OutputParamManager pManager) {
-      pManager.AddGenericParameter("Rebar", "Rb", "Rebar (single or bundle) for AdSec Reinforcement",
-        GH_ParamAccess.item);
-    }
-
-    protected override void SolveInternal(IGH_DataAccess DA) {
-      // 0 material input
-      var material = this.GetAdSecMaterial(DA, 0);
-
-      switch (_mode) {
-        case FoldMode.Single:
-          var rebar = new AdSecRebarBundleGoo(IBarBundle.Create((IReinforcement)material.Material,
-            (Length)Input.UnitNumber(this, DA, 1, _lengthUnit)));
-          DA.SetData(0, rebar);
-          break;
-
-        case FoldMode.Bundle:
-          int count = 1;
-          DA.GetData(2, ref count);
-
-          var bundle = new AdSecRebarBundleGoo(IBarBundle.Create((IReinforcement)material.Material,
-            (Length)Input.UnitNumber(this, DA, 1, _lengthUnit), count));
-
-          DA.SetData(0, bundle);
-          break;
-      }
-    }
-
-    protected override void UpdateUIFromSelectedItems() {
-      _mode = (FoldMode)Enum.Parse(typeof(FoldMode), _selectedItems[0]);
-      _lengthUnit = (LengthUnit)UnitsHelper.Parse(typeof(LengthUnit), _selectedItems[1]);
-      ToggleInput();
-      base.UpdateUIFromSelectedItems();
-    }
-
-    private void ToggleInput() {
-      RecordUndoEvent("Changed dropdown");
-      switch (_mode) {
-        case FoldMode.Single:
-          // remove any additional input parameters
-          while (Params.Input.Count > 2) {
-            Params.UnregisterInputParameter(Params.Input[2], true);
-          }
-
-          break;
-
-        case FoldMode.Bundle:
-          // add input parameter
-          while (Params.Input.Count != 3) {
-            Params.RegisterInputParam(new Param_Integer());
-          }
-
-          break;
-      }
-    }
-
-    private enum FoldMode {
-      Single,
-      Bundle,
+    private RebarMode UpdateMode() {
+      return (RebarMode)Enum.Parse(typeof(RebarMode), _selectedItems[0]);
     }
   }
 }
