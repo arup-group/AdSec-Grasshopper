a0bc9af6e3a4d67c91ad6b33b1cc2f73ba0daa26
Sandeep Kumar <sandeep.kumar@arup.com>
2025-05-23 13:04:21 +0530

    test(CreateRebarTests): check unit after compute

--- AdSecGHTests/Components/3_Rebar/CreateRebarTests.cs
+++ AdSecGHTests/Components/3_Rebar/CreateRebarTests.cs
@@ -42,6 +42,7 @@ namespace AdSecGHTests.Components._3_Rebar {
     [Fact]
     public void ShouldHaveTheUnitsChanged() {
       component.SetSelected(1, 0);
+      ComponentTestHelper.ComputeData(component);
       Assert.Contains("[mm]", component.BusinessComponent.DiameterParameter.Name);
     }
 
