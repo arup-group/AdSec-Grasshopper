3eb3c59c5eb48677e013455948f9dec45579f1ab
Sandeep Kumar <sandeep.kumar@arup.com>
2025-05-28 11:08:16 +0530

    test(EditSectionTests,CreateStandardMaterialTest): test added to ensure edit section can handle AdSecDesignCodeGoo as well as DesignCode

---
+++ AdSecGHTests/Components/1_Properties/CreateDesignCodeTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using AdSecGH;
+using AdSecGH.Components;
+using AdSecGH.Helpers;
+using AdSecGH.Parameters;
+using AdSecGH.Properties;
+
+using AdSecGHTests.Helpers;
+
+using Oasys.AdSec.DesignCode;
+using Oasys.GH.Helpers;
+
+using Xunit;
+
+namespace AdSecGHTests.Components.Properties {
+  [Collection("GrasshopperFixture collection")]
+  public class CreateDesignCodeTests {
+    private readonly CreateDesignCode _component;
+    public CreateDesignCodeTests() {
+      AdSecUtility.LoadAdSecAPI();
+      _component = new CreateDesignCode();
+    }
+
+    private void SetACI318Edition2002Metrric() {
+      _component.SetSelected(0, 1);
+    }
+
+    [Fact]
+    public void ExpectedDesignCodeIsACI318Edition2002Metric() {
+      SetACI318Edition2002Metrric();
+      var output = (AdSecDesignCodeGoo)ComponentTestHelper.GetOutput(_component);
+      Assert.Equal("ACI318+Edition_2002+Metric", output.Value.DesignCodeName);
+    }
+
+  }
+}
