f52d3ace4884cacf0b5cb450a164c03b617191ee
Sandeep Kumar <sandeep.kumar@arup.com>
2025-05-28 11:47:17 +0530

    test(CreateDesignCodeTests): DesignCode created from designCode component should be consistent with one created from standard material components(concrete)

--- AdSecGHTests/Components/1_Properties/CreateDesignCodeTests.cs
+++ AdSecGHTests/Components/1_Properties/CreateDesignCodeTests.cs
@@ -1,17 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-
-using AdSecGH;
-using AdSecGH.Components;
-using AdSecGH.Helpers;
+﻿using AdSecGH.Components;
 using AdSecGH.Parameters;
-using AdSecGH.Properties;
 
 using AdSecGHTests.Helpers;
-
-using Oasys.AdSec.DesignCode;
-using Oasys.GH.Helpers;
 
 using Xunit;
 
@@ -22,17 +12,31 @@ namespace AdSecGHTests.Components.Properties {
     public CreateDesignCodeTests() {
       AdSecUtility.LoadAdSecAPI();
       _component = new CreateDesignCode();
+      SetACI318Edition2002Metrric();
     }
 
     private void SetACI318Edition2002Metrric() {
       _component.SetSelected(0, 1);
     }
 
+    private AdSecMaterialGoo GetDesignCodeFromMaterial() {
+      var component = new CreateStandardMaterial();
+      component.SetSelected(0, 0);
+      component.SetSelected(1, 0);
+      return (AdSecMaterialGoo)ComponentTestHelper.GetOutput(component);
+    }
+
     [Fact]
     public void ExpectedDesignCodeIsACI318Edition2002Metric() {
-      SetACI318Edition2002Metrric();
       var output = (AdSecDesignCodeGoo)ComponentTestHelper.GetOutput(_component);
       Assert.Equal("ACI318+Edition_2002+Metric", output.Value.DesignCodeName);
+    }
+
+    [Fact]
+    public void DesignCodeCreatedFromDesignCodeOrThroughConcreteMaterialShouldBeConsistent() {
+      var designFromDesignCode = ((AdSecDesignCodeGoo)ComponentTestHelper.GetOutput(_component)).Value;
+      var designCodeFromMaterial = GetDesignCodeFromMaterial().Value.DesignCode;
+      Assert.Equal(designCodeFromMaterial.DesignCodeName, designFromDesignCode.DesignCodeName);
     }
 
   }
