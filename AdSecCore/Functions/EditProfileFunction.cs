6317390934f2cdf56dbc789caf69f2d125e81c06
spsarras <psarras.st@gmail.com>
2025-05-30 12:21:38 +0100

    test(EditProfileFunction): metadata and organisation on new function

---
+++ AdSecCore/Functions/EditProfileFunction.cs
@@ -0,0 +1,17 @@
+﻿using AdSecGHCore.Constants;
+
+namespace AdSecCore.Functions {
+  public class EditProfileFunction : Function {
+
+    public override FuncAttribute Metadata { get; set; } = new FuncAttribute() {
+      Name = "Edit Profile",
+      NickName = "ProfileEdit",
+      Description = "Modify an AdSec Profile",
+    };
+    public override Organisation Organisation { get; set; } = new Organisation() {
+      Category = CategoryName.Name(),
+      SubCategory = SubCategoryName.Cat2()
+    };
+    public override void Compute() { }
+  }
+}
---
+++ AdSecCoreTests/Functions/EditProfileFunctionTests.cs
@@ -0,0 +1,39 @@
+﻿using AdSecCore.Functions;
+
+using AdSecGHCore.Constants;
+
+namespace AdSecCoreTests.Functions {
+  public class EditProfileFunctionTests {
+
+    private EditProfileFunction _function;
+
+    public EditProfileFunctionTests() {
+      _function = new EditProfileFunction();
+    }
+
+    [Fact]
+    public void ShouldHaveAName() {
+      Assert.Equal("Edit Profile", _function.Metadata.Name);
+    }
+
+    [Fact]
+    public void ShouldHaveANickName() {
+      Assert.Equal("ProfileEdit", _function.Metadata.NickName);
+    }
+
+    [Fact]
+    public void ShouldHaveADescription() {
+      Assert.Equal("Modify an AdSec Profile", _function.Metadata.Description);
+    }
+
+    [Fact]
+    public void ShouldHaveACategory() {
+      Assert.Equal(CategoryName.Name(), _function.Organisation.Category);
+    }
+
+    [Fact]
+    public void ShouldHaveNoSubCategory() {
+      Assert.Equal(SubCategoryName.Cat2(), _function.Organisation.SubCategory);
+    }
+  }
+}
