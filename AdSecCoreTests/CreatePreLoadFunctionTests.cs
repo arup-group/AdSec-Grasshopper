ffe7a5c4438b430103be2867700ebaf54f1fb7ca
SandeepArup <61278421+SandeepArup@users.noreply.github.com>
2025-05-30 16:57:39 +0530

    ADSECGH-142 : Update create prestress component (#181)
    * refactor(CreatePreLoad):Update create prestress component
    
    * feat(CreatePreLoadFunction): parse preload based on input type
    
    * test(CreatePreLoadFunctionTests): add test for CreatePreLoadFunction
    
    * refactor(CreatePreLoadFunctionTests): refactor code to fix failing test
    
    * test(CreatePreLoadFunctionTests): Add test to check if bar template is null
    
    * test(CreatePreLoadTests): Add GH test for create pre-load
    
    * fix(AdapterBase): do not process output on error
    
    * fix(AdSecCore/AdSecRebarGroupGoo): create new instance of AdSecRebarGroup
    
    * test(CreatePreLoadTests): additional test to validate longitudinal reinforcement
    
    * refactor(CreatePreLoadFunction): refactor code as per review comment
    
    * test(CreatePreLoadFunctionTests): minor change to call just build as default is top
    
    * refactor(AdSecRebarGroup): review comment implemented
    
    * refactor(CreatePreLoadFunction): refactor code and move common string in CreatePreLoadFunction
    
    * refactor(DropdownAdapter): refactor code to reduce logic in GH component
    
    * refactor(CreatePreLoad): remove unused variable
    
    * refactor(DropdownAdapter): remove function argument
    
    * refactor(CreatePreLoadFunction): catch specific InvalidCastException and add into error message
    
    * test(CreatePreLoadFunctionTests): update failing test
    
    * fix(CreateRebarFunction): deleted file added back to repo
    
    * test(CreatePreLoadFunctionTests): set default value for PreLoadType in constructor

---
+++ AdSecCoreTests/CreatePreLoadFunctionTests.cs
@@ -0,0 +1,144 @@
+using AdSecCore.Builders;
+using AdSecCore.Functions;
+
+using AdSecGH.Parameters;
+
+using Oasys.AdSec.Reinforcement.Groups;
+using Oasys.AdSec.Reinforcement.Preloads;
+
+namespace AdSecCoreTests.Functions {
+  public class CreatePreLoadFunctionTests {
+    private readonly CreatePreLoadFunction _function;
+
+    public CreatePreLoadFunctionTests() {
+      _function = new CreatePreLoadFunction();
+      _function.PreLoadType = (PreLoadType)100;
+      _function.RebarGroupInput.Value = new AdSecRebarGroup(new BuilderTemplateGroup().Build());
+      _function.PreloadInput.Value = 10;
+    }
+
+    [Fact]
+    public void ComputeWithValidForceCreatesPreload() {
+      _function.PreLoadType = PreLoadType.Force;
+      _function.Compute();
+      var outPreLoad = (IPreForce)(((ILongitudinalGroup)_function.PreloadedRebarGroupOutput.Value.Group).Preload);
+      double outputForce = outPreLoad.Force.As(_function.ForceUnit);
+      Assert.Equal(10, outputForce);
+    }
+
+    [Fact]
+    public void ComputeWithValidStrainCreatesPreload() {
+      _function.PreLoadType = PreLoadType.Strain;
+      _function.Compute();
+      var outPreLoad = (IPreStrain)(((ILongitudinalGroup)_function.PreloadedRebarGroupOutput.Value.Group).Preload);
+      double outputForce = outPreLoad.Strain.As(_function.MaterialStrainUnit);
+      Assert.Equal(10, outputForce);
+    }
+
+    [Fact]
+    public void ComputeWithValidPreStressCreatesPreload() {
+      _function.PreLoadType = PreLoadType.Stress;
+      _function.Compute();
+      var outPreLoad = (IPreStress)(((ILongitudinalGroup)_function.PreloadedRebarGroupOutput.Value.Group).Preload);
+      double outputForce = outPreLoad.Stress.As(_function.StressUnitResult);
+      Assert.Equal(10, outputForce);
+    }
+
+    [Fact]
+    public void GetAllInputAttributesReturnsTwoParameters() {
+      var inputs = _function.GetAllInputAttributes();
+      Assert.Equal(2, inputs.Length);
+    }
+
+    [Fact]
+    public void GetAllOutputAttributesReturnsOneParameter() {
+      var outputs = _function.GetAllOutputAttributes();
+      Assert.Single(outputs);
+    }
+
+    [Fact]
+    public void ParametersHaveCorrectNames() {
+      Assert.Equal("RebarGroup", _function.RebarGroupInput.Name);
+      Assert.Equal("Force", _function.PreloadInput.Name);
+      Assert.Equal("Prestressed RebarGroup", _function.PreloadedRebarGroupOutput.Name);
+    }
+
+    [Fact]
+    public void ParametersHaveCorrectNickNames() {
+      Assert.Equal("RbG", _function.RebarGroupInput.NickName);
+      Assert.Equal("P", _function.PreloadInput.NickName);
+      Assert.Equal("RbG", _function.PreloadedRebarGroupOutput.NickName);
+    }
+
+    [Fact]
+    public void ParametersHaveCorrectDescriptions() {
+      Assert.Contains("Reinforcement Group to apply Preload", _function.RebarGroupInput.Description);
+      Assert.Contains("The preload value", _function.PreloadInput.Description);
+      Assert.Contains("Preloaded Rebar Group for AdSec Section", _function.PreloadedRebarGroupOutput.Description);
+    }
+
+    [Fact]
+    public void ParametersDataAccessAreCorrect() {
+      Assert.Equal(Access.Item, _function.RebarGroupInput.Access);
+      Assert.Equal(Access.Item, _function.PreloadInput.Access);
+      Assert.Equal(Access.Item, _function.PreloadedRebarGroupOutput.Access);
+    }
+
+    [Fact]
+    public void OrganisationHasCorrectValues() {
+      Assert.Equal("AdSec", _function.Organisation.Category);
+      Assert.Equal("Loads", _function.Organisation.SubCategory.Trim());
+    }
+
+    [Fact]
+    public void MetaDataHasCorrectValues() {
+      Assert.Equal("Create Prestress Load", _function.Metadata.Name);
+      Assert.Equal("Prestress", _function.Metadata.NickName);
+      Assert.Contains("Create an AdSec Prestress Load", _function.Metadata.Description);
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(-50)]
+    [InlineData(0)]
+    public void ComputeWithDifferentPreloadValuesCreatesCorrectPreload(double preloadValue) {
+      _function.PreLoadType = PreLoadType.Force;
+      _function.PreloadInput.Value = preloadValue;
+      _function.Compute();
+
+      var outPreLoad = (IPreForce)(((ILongitudinalGroup)_function.PreloadedRebarGroupOutput.Value.Group).Preload);
+      Assert.Equal(preloadValue, outPreLoad.Force.As(_function.ForceUnit), 6);
+    }
+
+    [Theory]
+    [InlineData("Force")]
+    [InlineData("Stress")]
+    [InlineData("Strain")]
+    public void CanUpdateParameterNameAndNickName(string forceType) {
+      _function.PreLoadType = (PreLoadType)Enum.Parse(typeof(PreLoadType), forceType, true); ;
+      Assert.Contains(forceType, _function.PreloadInput.Name);
+    }
+
+    [Fact]
+    public void ComputeReportErrorForInvalidRebarGroup() {
+      _function.RebarGroupInput.Value = null;
+      _function.Compute();
+      Assert.Single(_function.ErrorMessages);
+    }
+
+    [Fact]
+    public void ComputeReportErrorForInvalidPreloadType() {
+      _function.PreloadInput.Value = "InvalidType";
+      _function.Compute();
+      Assert.Single(_function.ErrorMessages);
+    }
+
+    [Fact]
+    public void ComputeReportErrorMessageWhenRebarTemplateIsNull() {
+      _function.RebarGroupInput.Value = new AdSecRebarGroup();
+      _function.Compute();
+      Assert.Single(_function.ErrorMessages);
+      Assert.Contains("Invalid RebarGroup input", _function.ErrorMessages[0]);
+    }
+  }
+}
---
+++ AdSecGHTests/Components/4_Loads/CreatePreLoadTests.cs
@@ -0,0 +1,131 @@
+using System;
+
+using AdSecCore;
+using AdSecCore.Builders;
+
+using AdSecGH;
+using AdSecGH.Components;
+using AdSecGH.Parameters;
+using AdSecGH.Properties;
+
+using AdSecGHTests.Helpers;
+
+using Grasshopper.Kernel;
+
+using Oasys.AdSec.Reinforcement.Groups;
+using Oasys.AdSec.Reinforcement.Preloads;
+using Oasys.GH.Helpers;
+
+using OasysGH.Units;
+
+using Xunit;
+
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AdSecGHTests.Components {
+  [Collection("GrasshopperFixture collection")]
+  public class CreatePreLoadTests {
+    private readonly CreatePreLoad _component;
+    private readonly DoubleComparer _comparer = new DoubleComparer();
+    private readonly AdSecRebarGroupGoo _rebarGroup = new AdSecRebarGroupGoo(new AdSecRebarGroup(new BuilderTemplateGroup().AtFace(ITemplateGroup.Face.Top).Build()));
+    public CreatePreLoadTests() {
+      _component = new CreatePreLoad();
+    }
+
+    private void SetDefaultInputs() {
+      ComponentTestHelper.SetInput(_component, _rebarGroup, 0);
+      ComponentTestHelper.SetInput(_component, 10, 1);
+    }
+
+    [Fact]
+    public void ShouldCreatePreLoadCorrectly() {
+      SetDefaultInputs();
+      ComponentTestHelper.ComputeData(_component);
+      var output = ((AdSecRebarGroupGoo)ComponentTestHelper.GetOutput(_component, 0));
+      var outPreLoad = (IPreForce)((ILongitudinalGroup)output.Value.Group).Preload;
+      Assert.NotNull(output);
+      Assert.NotNull(output.Value.Group as ILongitudinalGroup);
+      Assert.Equal(10, outPreLoad.Force.As(DefaultUnits.ForceUnit), _comparer);
+    }
+
+
+    [Fact]
+    public void ShouldThrowExceptionWhenRebarGroupIsNull() {
+      ComponentTestHelper.SetInput(_component, new AdSecRebarGroupGoo(), 0);
+      ComponentTestHelper.SetInput(_component, 10, 1);
+      Assert.Throws<NullReferenceException>(() => ComponentTestHelper.ComputeData(_component));
+    }
+
+    [Fact]
+    public void ShouldLogErrorMessageWhenRebarGroupInputIsNull() {
+      AdSecRebarGroupGoo group = null;
+      ComponentTestHelper.SetInput(_component, group, 0);
+      ComponentTestHelper.SetInput(_component, 10, 1);
+      ComponentTestHelper.ComputeData(_component);
+      var runtimeMessages = _component.RuntimeMessages(GH_RuntimeMessageLevel.Error);
+      Assert.Single(runtimeMessages);
+      Assert.Contains("Invalid RebarGroup input", runtimeMessages[0]);
+    }
+
+    [Fact]
+    public void ShouldThrowExceptionWhenPreloadInputIsInvalid() {
+      ComponentTestHelper.SetInput(_component, _rebarGroup, 0);
+      ComponentTestHelper.SetInput(_component, "InvalidType", 1);
+      ComponentTestHelper.ComputeData(_component);
+      var runtimeMessages = _component.RuntimeMessages(GH_RuntimeMessageLevel.Error);
+      Assert.Single(runtimeMessages);
+      Assert.Contains("Could not parse the input InvalidType to the desired quantity", runtimeMessages[0]);
+    }
+
+    [Fact]
+    public void ShouldLogErrorMessageWhenRebarGroupIsNotLogitudinal() {
+      var linkGroup = ILinkGroup.Create(new BuilderLayer().Build().BarBundle);
+      var rebarGroup = new AdSecRebarGroupGoo(new AdSecRebarGroup(linkGroup));
+      ComponentTestHelper.SetInput(_component, rebarGroup, 0);
+      ComponentTestHelper.SetInput(_component, 10, 1);
+      ComponentTestHelper.ComputeData(_component);
+      var runtimeMessages = _component.RuntimeMessages(GH_RuntimeMessageLevel.Error);
+      Assert.Single(runtimeMessages);
+      Assert.Contains("RebarGroup must be a longitudinal group", runtimeMessages[0]);
+    }
+
+    [Fact]
+    public void ShouldUpdateForceUnitsCorrectly() {
+      _component.SetSelected(0, 0);
+      _component.SetSelected(1, 3);
+      ComponentTestHelper.ComputeData(_component);
+      Assert.Contains("Force [lbf]", _component.Params.Input[1].Name);
+    }
+
+    [Fact]
+    public void ShouldUpdateStrainUnitsCorrectly() {
+      _component.SetSelected(0, 1);
+      _component.SetSelected(1, 3);
+      ComponentTestHelper.ComputeData(_component);
+      Assert.Contains("Strain [%]", _component.Params.Input[1].Name);
+    }
+
+    [Fact]
+    public void ShouldUpdateStressUnitsCorrectly() {
+      _component.SetSelected(0, 2);
+      _component.SetSelected(1, 3);
+      ComponentTestHelper.ComputeData(_component);
+      Assert.Contains("Stress [GPa]", _component.Params.Input[1].Name);
+    }
+
+    [Fact]
+    public void ShouldHaveIconReferenced() {
+      Assert.True(_component.MatchesExpectedIcon(Resources.Prestress));
+    }
+
+    [Fact]
+    public void ShouldHavePluginInfoReferenced() {
+      Assert.Equal(PluginInfo.Instance, _component.PluginInfo);
+    }
+
+    [Fact]
+    public void ShouldBeHidden() {
+      Assert.False(_component.Hidden);
+    }
+  }
+}
