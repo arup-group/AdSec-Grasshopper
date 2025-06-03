7126d45e54ef898532b20b4e99f3cc182c38f43d
spsarras <psarras.st@gmail.com>
2025-06-03 16:37:52 +0100

    test(CreateRebarSpacingTests): icons and plugininfo

---
+++ AdSecGHTests/Components/3_Rebar/CreateRebarSpacingTests.cs
@@ -0,0 +1,28 @@
+﻿using AdSecGH;
+using AdSecGH.Components;
+using AdSecGH.Properties;
+
+using Oasys.GH.Helpers;
+
+using Xunit;
+
+namespace AdSecGHTests.Components._3_Rebar {
+  [Collection("GrasshopperFixture collection")]
+  public class CreateRebarSpacingTests {
+    private readonly CreateRebarSpacing component;
+
+    public CreateRebarSpacingTests() {
+      component = new CreateRebarSpacing();
+    }
+
+    [Fact]
+    public void ShouldHavePluginInfoReferenced() {
+      Assert.Equal(PluginInfo.Instance, component.PluginInfo);
+    }
+
+    [Fact]
+    public void ShouldHaveIconReferenced() {
+      Assert.True(component.MatchesExpectedIcon(Resources.RebarSpacing));
+    }
+  }
+}
