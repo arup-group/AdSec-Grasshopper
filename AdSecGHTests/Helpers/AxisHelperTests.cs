880f42190da6dbe3c1c641faf80dfd3b08f6a3ff
DominikaLos <dominika.los@arup.com>
2025-06-04 10:47:53 +0200

    refactor: modified tests

--- AdSecGHTests/Helpers/AxisHelperTests.cs
+++ AdSecGHTests/Helpers/AxisHelperTests.cs
@@ -13,7 +13,7 @@ namespace AdSecGHTests.Helpers {
       var plane = Plane.WorldXY;
       var (x, y, z) = AxisHelper.GetLocalAxisLines(profile, plane);
 
-      double expectedLength = 42.267; // meters
+      double expectedLength = 0.108; //defaultUnits
 
       Assert.Equal(expectedLength, x.Length, 3);
       Assert.Equal(expectedLength, y.Length, 3);
