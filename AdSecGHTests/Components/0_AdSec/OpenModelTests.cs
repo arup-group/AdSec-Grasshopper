3ee2b5b86a8e086c6d8c628cd3cb2c4b658e7eb0
spsarras <psarras.st@gmail.com>
2025-06-05 14:25:55 +0100

    test(OpenModelTests): for simple properties and CreateAttributes()

---
+++ AdSecGHTests/Components/0_AdSec/OpenModelTests.cs
@@ -0,0 +1,33 @@
+﻿using AdSecGH;
+using AdSecGH.Components;
+using AdSecGH.Properties;
+
+using Oasys.GH.Helpers;
+
+using Xunit;
+
+[Collection("GrasshopperFixture collection")]
+public class OpenModelTests {
+  private OpenModel _component;
+
+  public OpenModelTests() {
+    _component = new OpenModel();
+  }
+
+  [Fact]
+  public void ShouldHavePluginInfoReferenced() {
+    Assert.Equal(PluginInfo.Instance, _component.PluginInfo);
+  }
+
+  [Fact]
+  public void ShouldHaveIconReferenced() {
+    Assert.True(_component.MatchesExpectedIcon(Resources.OpenAdSec));
+  }
+
+  [Fact]
+  public void ShouldInitializeAttributes() {
+    _component.Attributes = null;
+    _component.CreateAttributes();
+    Assert.NotNull(_component.Attributes);
+  }
+}
