489fc8a2d5b7d2e6c463ac43477ad1d5792706ea
spsarras <psarras.st@gmail.com>
2025-06-05 14:19:32 +0100

    test(OpenModelFunction): organisation and metadata

---
+++ AdSecCore/Functions/OpenModelFunction.cs
@@ -0,0 +1,17 @@
+﻿using AdSecCore.Functions;
+
+using AdSecGHCore.Constants;
+
+public class OpenModelFunction : Function {
+
+  public override FuncAttribute Metadata { get; set; } = new FuncAttribute() {
+    Description = "Open an existing AdSec .ads file",
+    Name = "Open Model",
+    NickName = "Open",
+  };
+  public override Organisation Organisation { get; set; } = new Organisation() {
+    Category = CategoryName.Name(),
+    SubCategory = SubCategoryName.Cat0(),
+  };
+  public override void Compute() { }
+}
---
+++ AdSecCoreTests/Functions/OpenModelFunctionTests.cs
@@ -0,0 +1,37 @@
+﻿using AdSecGHCore.Constants;
+
+namespace AdSecCoreTests.Functions {
+  public class OpenModelFunctionTests {
+    private OpenModelFunction _function;
+
+    public OpenModelFunctionTests() {
+      _function = new OpenModelFunction();
+    }
+
+    [Fact]
+    public void ShouldHaveAName() {
+      Assert.Equal("Open Model", _function.Metadata.Name);
+    }
+
+    [Fact]
+    public void ShouldHaveANickName() {
+      Assert.Equal("Open", _function.Metadata.NickName);
+    }
+
+    [Fact]
+    public void ShouldHaveADescription() {
+      Assert.Equal("Open an existing AdSec .ads file", _function.Metadata.Description);
+    }
+
+    [Fact]
+    public void ShouldHaveACategory() {
+      Assert.Equal(CategoryName.Name(), _function.Organisation.Category);
+    }
+
+    [Fact]
+    public void ShouldHaveNoSubCategory() {
+      Assert.Equal(SubCategoryName.Cat0(), _function.Organisation.SubCategory);
+    }
+
+  }
+}
