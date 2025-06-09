35dde4b12b9e129bb9b308f91014a2ea42befd1f
spsarras <psarras.st@gmail.com>
2025-06-09 17:35:35 +0100

    refactor(DialogGhTests): moved to separate file

---
+++ AdSecGHTests/Components/0_AdSec/DialogGhTests.cs
@@ -0,0 +1,34 @@
+﻿using AdSecGH.Components;
+
+using Rhino.UI;
+
+using Xunit;
+
+namespace AdSecGHTests.Components {
+  [Collection("GrasshopperFixture collection")]
+  public class DialogGhTests {
+    private readonly DialogGh _dialog;
+
+    public DialogGhTests() {
+      _dialog = new DialogGh();
+    }
+
+    [Fact]
+    public void ShouldReturnFileNameFromDialog() {
+      var dialog = new OpenFileDialog();
+      dialog.FileName = "test.adsec";
+      var _dialog = new DialogGh(dialog);
+      Assert.Equal("test.adsec", _dialog.FileName);
+    }
+
+    [Fact]
+    public void ShouldOnlyWorkWithAdsFiles() {
+      Assert.Equal("AdSec Files(*.ads)|*.ads|All files (*.*)|*.*", _dialog._openFileDialog.Filter);
+    }
+
+    [Fact]
+    public void ShouldHaveATitle() {
+      Assert.Equal("Open AdSec Model", _dialog._openFileDialog.Title);
+    }
+  }
+}
--- AdSecGHTests/Components/0_AdSec/OpenModelTests.cs
+++ AdSecGHTests/Components/0_AdSec/OpenModelTests.cs
@@ -7,38 +7,9 @@ using Grasshopper.Kernel.Special;
 
 using Oasys.GH.Helpers;
 
-using Rhino.UI;
-
 using Xunit;
 
 namespace AdSecGHTests.Components {
-
-  [Collection("GrasshopperFixture collection")]
-  public class DialogGhTests {
-    private readonly DialogGh _dialog;
-
-    public DialogGhTests() {
-      _dialog = new DialogGh();
-    }
-
-    [Fact]
-    public void ShouldReturnFileNameFromDialog() {
-      var dialog = new OpenFileDialog();
-      dialog.FileName = "test.adsec";
-      var _dialog = new DialogGh(dialog);
-      Assert.Equal("test.adsec", _dialog.FileName);
-    }
-
-    [Fact]
-    public void ShouldOnlyWorkWithAdsFiles() {
-      Assert.Equal("AdSec Files(*.ads)|*.ads|All files (*.*)|*.*", _dialog._openFileDialog.Filter);
-    }
-
-    [Fact]
-    public void ShouldHaveATitle() {
-      Assert.Equal("Open AdSec Model", _dialog._openFileDialog.Title);
-    }
-  }
 
   [Collection("GrasshopperFixture collection")]
   public class OpenModelTests {
