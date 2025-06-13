1e0b8802700a2b5a2fc4950866beafec8f2d3726
spsarras <psarras.st@gmail.com>
2025-06-13 14:33:29 +0100

    fix(CreateCustomMaterial): added namespaces

--- AdSecCore/Functions/CreateCustomMaterialFunction.cs
+++ AdSecCore/Functions/CreateCustomMaterialFunction.cs
@@ -3,61 +3,64 @@ using AdSecCore.Parameters;
 
 using AdSecGHCore.Constants;
 
-public class CreateCustomMaterialFunction : Function {
+namespace AdSecCore.Functions {
 
-  public override FuncAttribute Metadata { get; set; } = new FuncAttribute() {
-    Name = "Custom Material",
-    NickName = "CustomMaterial",
-    Description = "Create a custom AdSec Material",
-  };
-  public override Organisation Organisation { get; set; } = new Organisation() {
-    Category = CategoryName.Name(),
-    SubCategory = SubCategoryName.Cat1()
-  };
+  public class CreateCustomMaterialFunction : Function {
 
-  public DesignCodeParameter DesignCode { get; set; }
-    = Default.DesignCode(optional: true, description: "[Optional] Set the Material's DesignCode");
-
-  public StressStrainCurveParameter UlsCompressionCurve { get; set; } = new StressStrainCurveParameter() {
-    Name = "ULS Comp. Crv",
-    NickName = "U_C",
-    Description = "ULS Stress Strain Curve for Compression",
-  };
-
-  public StressStrainCurveParameter UlsTensionCurve { get; set; } = new StressStrainCurveParameter() {
-    Name = "ULS Tens. Crv",
-    NickName = "U_T",
-    Description = "ULS Stress Strain Curve for Tension",
-  };
-
-  public StressStrainCurveParameter SlsCompressionCurve { get; set; } = new StressStrainCurveParameter() {
-    Name = "SLS Comp. Crv",
-    NickName = "S_C",
-    Description = "SLS Stress Strain Curve for Compression",
-  };
-
-  public StressStrainCurveParameter SlsTensionCurve { get; set; } = new StressStrainCurveParameter() {
-    Name = "SLS Tens. Crv",
-    NickName = "S_T",
-    Description = "SLS Stress Strain Curve for Tension",
-  };
-  public CrackCalcParameter CrackCalcParams { get; set; } = new CrackCalcParameter() {
-    Name = "Crack Calc Params",
-    NickName = "CCP",
-    Description = "[Optional] Material's Crack Calculation Parameters",
-    Optional = true,
-  };
-
-  public override Attribute[] GetAllInputAttributes() {
-    return new Attribute[] {
-      DesignCode,
-      UlsCompressionCurve,
-      UlsTensionCurve,
-      SlsCompressionCurve,
-      SlsTensionCurve,
-      CrackCalcParams,
+    public override FuncAttribute Metadata { get; set; } = new FuncAttribute() {
+      Name = "Custom Material",
+      NickName = "CustomMaterial",
+      Description = "Create a custom AdSec Material",
     };
-  }
+    public override Organisation Organisation { get; set; } = new Organisation() {
+      Category = CategoryName.Name(),
+      SubCategory = SubCategoryName.Cat1()
+    };
 
-  public override void Compute() { throw new System.NotImplementedException(); }
+    public DesignCodeParameter DesignCode { get; set; }
+      = Default.DesignCode(optional: true, description: "[Optional] Set the Material's DesignCode");
+
+    public StressStrainCurveParameter UlsCompressionCurve { get; set; } = new StressStrainCurveParameter() {
+      Name = "ULS Comp. Crv",
+      NickName = "U_C",
+      Description = "ULS Stress Strain Curve for Compression",
+    };
+
+    public StressStrainCurveParameter UlsTensionCurve { get; set; } = new StressStrainCurveParameter() {
+      Name = "ULS Tens. Crv",
+      NickName = "U_T",
+      Description = "ULS Stress Strain Curve for Tension",
+    };
+
+    public StressStrainCurveParameter SlsCompressionCurve { get; set; } = new StressStrainCurveParameter() {
+      Name = "SLS Comp. Crv",
+      NickName = "S_C",
+      Description = "SLS Stress Strain Curve for Compression",
+    };
+
+    public StressStrainCurveParameter SlsTensionCurve { get; set; } = new StressStrainCurveParameter() {
+      Name = "SLS Tens. Crv",
+      NickName = "S_T",
+      Description = "SLS Stress Strain Curve for Tension",
+    };
+    public CrackCalcParameter CrackCalcParams { get; set; } = new CrackCalcParameter() {
+      Name = "Crack Calc Params",
+      NickName = "CCP",
+      Description = "[Optional] Material's Crack Calculation Parameters",
+      Optional = true,
+    };
+
+    public override Attribute[] GetAllInputAttributes() {
+      return new Attribute[] {
+        DesignCode,
+        UlsCompressionCurve,
+        UlsTensionCurve,
+        SlsCompressionCurve,
+        SlsTensionCurve,
+        CrackCalcParams,
+      };
+    }
+
+    public override void Compute() { throw new System.NotImplementedException(); }
+  }
 }
--- AdSecCoreTests/Functions/CreateCustomMaterialFunctionTests.cs
+++ AdSecCoreTests/Functions/CreateCustomMaterialFunctionTests.cs
@@ -1,101 +1,106 @@
-﻿using AdSecGHCore.Constants;
+﻿using AdSecCore.Functions;
 
-public class CreateCustomMaterialFunctionTests {
-  CreateCustomMaterialFunction _function;
+using AdSecGHCore.Constants;
 
-  public CreateCustomMaterialFunctionTests() {
-    _function = new CreateCustomMaterialFunction();
-  }
+namespace AdSecCoreTests.Functions {
 
-  [Fact]
-  public void ShouldHaveAName() {
-    Assert.Equal("Custom Material", _function.Metadata.Name);
-  }
+  public class CreateCustomMaterialFunctionTests {
+    CreateCustomMaterialFunction _function;
 
-  [Fact]
-  public void ShouldHaveANickName() {
-    Assert.Equal("CustomMaterial", _function.Metadata.NickName);
-  }
+    public CreateCustomMaterialFunctionTests() {
+      _function = new CreateCustomMaterialFunction();
+    }
 
-  [Fact]
-  public void ShouldHaveADescription() {
-    Assert.Equal("Create a custom AdSec Material", _function.Metadata.Description);
-  }
+    [Fact]
+    public void ShouldHaveAName() {
+      Assert.Equal("Custom Material", _function.Metadata.Name);
+    }
 
-  [Fact]
-  public void ShouldHaveACategory() {
-    Assert.Equal(CategoryName.Name(), _function.Organisation.Category);
-  }
+    [Fact]
+    public void ShouldHaveANickName() {
+      Assert.Equal("CustomMaterial", _function.Metadata.NickName);
+    }
 
-  [Fact]
-  public void ShouldHaveNoSubCategory() {
-    Assert.Equal(SubCategoryName.Cat1(), _function.Organisation.SubCategory);
-  }
+    [Fact]
+    public void ShouldHaveADescription() {
+      Assert.Equal("Create a custom AdSec Material", _function.Metadata.Description);
+    }
 
-  [Fact]
-  public void ShouldHaveSixInputs() {
-    Assert.Equal(6, _function.GetAllInputAttributes().Length);
-  }
+    [Fact]
+    public void ShouldHaveACategory() {
+      Assert.Equal(CategoryName.Name(), _function.Organisation.Category);
+    }
 
-  [Fact]
-  public void ShouldHaveDesignCode() {
-    Assert.Equal("DesignCode", _function.DesignCode.Name);
-  }
+    [Fact]
+    public void ShouldHaveNoSubCategory() {
+      Assert.Equal(SubCategoryName.Cat1(), _function.Organisation.SubCategory);
+    }
 
-  [Fact]
-  public void ShouldExplainDesignCodeIsOptional() {
-    Assert.Equal("[Optional] Set the Material's DesignCode", _function.DesignCode.Description);
-  }
+    [Fact]
+    public void ShouldHaveSixInputs() {
+      Assert.Equal(6, _function.GetAllInputAttributes().Length);
+    }
 
-  [Fact]
-  public void ShouldHaveDesignCodeOptional() {
-    Assert.True(_function.DesignCode.Optional);
-  }
+    [Fact]
+    public void ShouldHaveDesignCode() {
+      Assert.Equal("DesignCode", _function.DesignCode.Name);
+    }
 
-  [Fact]
-  public void ShouldHaveUlsCompressionCrv() {
-    Assert.Equal("ULS Comp. Crv", _function.UlsCompressionCurve.Name);
-    Assert.Equal("U_C", _function.UlsCompressionCurve.NickName);
-    Assert.Equal("ULS Stress Strain Curve for Compression", _function.UlsCompressionCurve.Description);
-  }
+    [Fact]
+    public void ShouldExplainDesignCodeIsOptional() {
+      Assert.Equal("[Optional] Set the Material's DesignCode", _function.DesignCode.Description);
+    }
 
-  [Fact]
-  public void ShouldHaveUlsTensionCrv() {
-    Assert.Equal("ULS Tens. Crv", _function.UlsTensionCurve.Name);
-    Assert.Equal("U_T", _function.UlsTensionCurve.NickName);
-    Assert.Equal("ULS Stress Strain Curve for Tension", _function.UlsTensionCurve.Description);
-  }
+    [Fact]
+    public void ShouldHaveDesignCodeOptional() {
+      Assert.True(_function.DesignCode.Optional);
+    }
 
-  [Fact]
-  public void ShouldHaveSlsCompressionCrv() {
-    Assert.Equal("SLS Comp. Crv", _function.SlsCompressionCurve.Name);
-    Assert.Equal("S_C", _function.SlsCompressionCurve.NickName);
-    Assert.Equal("SLS Stress Strain Curve for Compression", _function.SlsCompressionCurve.Description);
-  }
+    [Fact]
+    public void ShouldHaveUlsCompressionCrv() {
+      Assert.Equal("ULS Comp. Crv", _function.UlsCompressionCurve.Name);
+      Assert.Equal("U_C", _function.UlsCompressionCurve.NickName);
+      Assert.Equal("ULS Stress Strain Curve for Compression", _function.UlsCompressionCurve.Description);
+    }
 
-  [Fact]
-  public void ShouldHaveSlsTensionCrv() {
-    Assert.Equal("SLS Tens. Crv", _function.SlsTensionCurve.Name);
-    Assert.Equal("S_T", _function.SlsTensionCurve.NickName);
-    Assert.Equal("SLS Stress Strain Curve for Tension", _function.SlsTensionCurve.Description);
-  }
+    [Fact]
+    public void ShouldHaveUlsTensionCrv() {
+      Assert.Equal("ULS Tens. Crv", _function.UlsTensionCurve.Name);
+      Assert.Equal("U_T", _function.UlsTensionCurve.NickName);
+      Assert.Equal("ULS Stress Strain Curve for Tension", _function.UlsTensionCurve.Description);
+    }
 
-  [Fact]
-  public void ShouldHaveOptionalCrackCalcParams() {
-    Assert.Equal("Crack Calc Params", _function.CrackCalcParams.Name);
-    Assert.Equal("CCP", _function.CrackCalcParams.NickName);
-    Assert.Equal("[Optional] Material's Crack Calculation Parameters", _function.CrackCalcParams.Description);
-    Assert.True(_function.CrackCalcParams.Optional);
-  }
+    [Fact]
+    public void ShouldHaveSlsCompressionCrv() {
+      Assert.Equal("SLS Comp. Crv", _function.SlsCompressionCurve.Name);
+      Assert.Equal("S_C", _function.SlsCompressionCurve.NickName);
+      Assert.Equal("SLS Stress Strain Curve for Compression", _function.SlsCompressionCurve.Description);
+    }
 
-  [Fact]
-  public void ShouldHaveInputsAtFollowingOrder() {
-    var inputs = _function.GetAllInputAttributes();
-    Assert.Equal(_function.DesignCode, inputs[0]);
-    Assert.Equal(_function.UlsCompressionCurve, inputs[1]);
-    Assert.Equal(_function.UlsTensionCurve, inputs[2]);
-    Assert.Equal(_function.SlsCompressionCurve, inputs[3]);
-    Assert.Equal(_function.SlsTensionCurve, inputs[4]);
-    Assert.Equal(_function.CrackCalcParams, inputs[5]);
+    [Fact]
+    public void ShouldHaveSlsTensionCrv() {
+      Assert.Equal("SLS Tens. Crv", _function.SlsTensionCurve.Name);
+      Assert.Equal("S_T", _function.SlsTensionCurve.NickName);
+      Assert.Equal("SLS Stress Strain Curve for Tension", _function.SlsTensionCurve.Description);
+    }
+
+    [Fact]
+    public void ShouldHaveOptionalCrackCalcParams() {
+      Assert.Equal("Crack Calc Params", _function.CrackCalcParams.Name);
+      Assert.Equal("CCP", _function.CrackCalcParams.NickName);
+      Assert.Equal("[Optional] Material's Crack Calculation Parameters", _function.CrackCalcParams.Description);
+      Assert.True(_function.CrackCalcParams.Optional);
+    }
+
+    [Fact]
+    public void ShouldHaveInputsAtFollowingOrder() {
+      var inputs = _function.GetAllInputAttributes();
+      Assert.Equal(_function.DesignCode, inputs[0]);
+      Assert.Equal(_function.UlsCompressionCurve, inputs[1]);
+      Assert.Equal(_function.UlsTensionCurve, inputs[2]);
+      Assert.Equal(_function.SlsCompressionCurve, inputs[3]);
+      Assert.Equal(_function.SlsTensionCurve, inputs[4]);
+      Assert.Equal(_function.CrackCalcParams, inputs[5]);
+    }
   }
 }
