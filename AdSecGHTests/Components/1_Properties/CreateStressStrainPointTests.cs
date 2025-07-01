fbdf6241ba0496c8d040f7c36760b8432c1a9990
Sandeep Kumar <sandeep.kumar@arup.com>
2025-07-01 13:27:58 +0530

    test(CreateStressStrainPointTests): test to read saved file

--- AdSecGHTests/Components/1_Properties/CreateStressStrainPointTests.cs
+++ AdSecGHTests/Components/1_Properties/CreateStressStrainPointTests.cs
@@ -5,12 +5,17 @@ using AdSecGH.Components;
 using AdSecGH.Parameters;
 using AdSecGH.Properties;
 
+using AdSecGHTests.Components._3_Rebar;
 using AdSecGHTests.Helpers;
+
+using Grasshopper.Kernel;
 
 using Oasys.GH.Helpers;
 
 using OasysUnits;
 using OasysUnits.Units;
+
+using Rhino.Commands;
 
 using Xunit;
 
@@ -37,10 +42,31 @@ namespace AdSecGHTests.Components._02_Properties {
     public void ComputeWithValidInputsShouldCreatePoint() {
       SetStrainInput();
       SetStressInput();
-      var result = (AdSecStressStrainPointGoo)ComponentTestHelper.GetOutput(_component);
+      AssertStressStrainPointOutput(_component);
+    }
+
+    private void AssertStressStrainPointOutput(CreateStressStrainPoint component) {
+      var result = (AdSecStressStrainPointGoo)ComponentTestHelper.GetOutput(component);
       Assert.NotNull(result);
       Assert.Equal(0.002, result.StressStrainPoint.Strain.As(StrainUnit.Ratio), new DoubleComparer());
       Assert.Equal(30000, result.StressStrainPoint.Stress.As(PressureUnit.Kilopascal), new DoubleComparer());
+    }
+
+    [Fact]
+    public void RectangleModelShouldUseSavedMode() {
+      SetStrainInput();
+      SetStressInput();
+      var doc = new GH_DocumentIO();
+      doc.Document = new GH_Document();
+      doc.Document.AddObject(_component, false);
+      var randomPath = CreateRebarGroupSaveLoadTests.GetRandomName();
+      doc.SaveQuiet(randomPath);
+      doc.Open(randomPath);
+      doc.Document.NewSolution(true);
+      var component = (CreateStressStrainPoint)doc.Document.FindComponent(_component.InstanceGuid);
+      ComponentTestHelper.SetInput(component, 30000, 1);
+      ComponentTestHelper.SetInput(component, 0.002, 0);
+      AssertStressStrainPointOutput(component);
     }
 
     private void SetStressInput() {
