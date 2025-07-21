51366151982229dbb08e85dc03698670bbf05840
Sandeep Kumar <sandeep.kumar@arup.com>
2025-07-21 13:52:39 +0530

    fix(BusinessExtensions): parse default value based on input type

--- AdSecGH/Helpers/BusinessExtensions.cs
+++ AdSecGH/Helpers/BusinessExtensions.cs
@@ -404,31 +404,8 @@ namespace Oasys.GH.Helpers {
             dynamic gooDynamic = goo;
             return new AdSecPointGoo(gooDynamic);
           }
-        }, {
-          typeof(DoubleParameter), goo => {
-            if (goo is double value) {
-              return value;
-            }
-
-            return null;
-          }
-        }, {
-          typeof(BooleanParameter), goo => {
-            if (goo is bool value) {
-              return value;
-            }
-
-            return null;
-          }
-        }, {
-          typeof(NullableDoubleParameter), goo => {
-            if (goo is double value) {
-              return value;
-            }
-
-            return null;
-          }
-        }, {
+        },
+        {
           typeof(DoubleArrayParameter), goo => {
             var list = goo as List<object>;
             return list.Select(x => {
