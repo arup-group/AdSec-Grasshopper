0114d6a18b222dfbc5da8d592d3f4b870d1d36c3
spsarras <psarras.st@gmail.com>
2025-08-07 16:12:40 +0100

    test(AdSecFileHelperTests): simple Load and LoadFrom Tests

---
+++ AdSecCoreTests/Constants/AdSecFileHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using AdSecCore.Constants;
+
+namespace AdSecCoreTests.Constants {
+  public class AdSecFileHelperTests {
+
+    [Fact]
+    public void ShouldLoadAssemblyUsingLoadFrom() {
+      var loader = new AdSecDllLoader(AdSecDllLoader.LoadMode.LoadFrom);
+      Assert.NotNull(loader.AdSecAPI());
+    }
+    [Fact]
+    public void ShouldLoadAssemblyUsingLoad() {
+      Assert.Throws<FileNotFoundException>(() => {
+        var loader = new AdSecDllLoader(AdSecDllLoader.LoadMode.Load);
+        Assert.NotNull(loader.AdSecAPI());
+      });
+    }
+  }
+}
