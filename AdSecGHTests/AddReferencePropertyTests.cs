f48fbb8205ef592331c92acee8bd4d3073d9479a
spsarras <psarras.st@gmail.com>
2025-08-07 15:45:50 +0100

    test(AddReferencePropertyTests): added simple tests check lines run properly

---
+++ AdSecGHTests/AddReferencePropertyTests.cs
@@ -0,0 +1,27 @@
+﻿using AdSecCore.Constants;
+
+using AdSecGH;
+
+using Xunit;
+
+namespace AdSecGHTests {
+  [Collection("GrasshopperFixture collection")]
+  public class AddReferencePropertyTests {
+
+    public AddReferencePropertyTests() {
+      var priority = new AddReferencePriority();
+      priority.PriorityLoad();
+    }
+
+    [Fact]
+    public void ShouldHaveAValidAdSecAssembly() {
+      Assert.NotNull(AddReferencePriority.AdSecAPI);
+    }
+
+    [Fact]
+    public void ShouldHaveTheSameReference() {
+      Assert.Equal(AddReferencePriority.AdSecAPI, AdSecFileHelper.Custom);
+    }
+
+  }
+}
