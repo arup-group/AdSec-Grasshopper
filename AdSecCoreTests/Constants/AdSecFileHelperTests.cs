6b6c30cf6fef35811cc6868e4b06d7e747bb03e3
Stamatios Psarras <psarras.st@gmail.com>
2025-08-08 08:49:16 +0100

    ADSECGH-188: Assembly Load Fix for Rhino 8 (#234)
    * fix(AdSecFileHelper): added custom Assembly Load to be used from GH on Rhino 8
    
    * test(AddReferencePropertyTests): added simple tests check lines run properly
    
    * test(AdSecFileHelperTests): simple Load and LoadFrom Tests
    
    * test(AdSecFileHelperTests): simple test for Custom

---
+++ AdSecCoreTests/Constants/AdSecFileHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using AdSecCore.Constants;
+
+namespace AdSecCoreTests.Constants {
+  public class AdSecFileHelperTests {
+
+    [Fact]
+    public void ShouldLoadAssemblyUsingLoadFrom() {
+      var loader = new AdSecDllLoader(AdSecDllLoader.LoadMode.LoadFrom);
+      Assert.NotNull(loader.AdSecAPI());
+    }
+
+    [Fact]
+    public void ShouldLoadAssemblyUsingLoad() {
+      Assert.Throws<FileNotFoundException>(() => {
+        var loader = new AdSecDllLoader(AdSecDllLoader.LoadMode.Load);
+        Assert.NotNull(loader.AdSecAPI());
+      });
+    }
+
+    [Fact]
+    public void ShouldLoadAssemblyUsingCustom() {
+      var loader = new AdSecDllLoader(AdSecDllLoader.LoadMode.Custom) {
+        Custom = null,
+      };
+      Assert.Null(loader.AdSecAPI());
+    }
+  }
+}
