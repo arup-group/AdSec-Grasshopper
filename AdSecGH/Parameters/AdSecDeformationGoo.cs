2dc6cc5d011c8415571d8f0e25064f7dac1d0eda
Stamatios Psarras <psarras.st@gmail.com>
2025-08-12 14:49:21 +0100

    ADSECGH-191: Deformation Goo to GH_Vector (#237)
    * fix(AdSecDeformationGoo): added cast to vector option
    
    * test(AdSecDeformationGooTests): simple tests to check casting to vector

--- AdSecGH/Parameters/AdSecDeformationGoo.cs
+++ AdSecGH/Parameters/AdSecDeformationGoo.cs
@@ -9,14 +9,15 @@ using OasysGH.Units;
 
 using OasysUnits;
 
+using Rhino.Geometry;
+
 namespace AdSecGH.Parameters {
   public class AdSecDeformationGoo : GH_Goo<IDeformation> {
     public override bool IsValid => true;
     public override string TypeDescription => $"AdSec {TypeName} Parameter";
     public override string TypeName => "Deformation";
 
-    public AdSecDeformationGoo(IDeformation deformation) : base(deformation) {
-    }
+    public AdSecDeformationGoo(IDeformation deformation) : base(deformation) { }
 
     public override IGH_Goo Duplicate() {
       return new AdSecDeformationGoo(Value);
@@ -28,6 +29,17 @@ namespace AdSecGH.Parameters {
       string curvatureUnitAbbreviation = string.Concat(curvature.ToString().Where(char.IsLetter));
       return
         $"AdSec {TypeName} {{{Math.Round(Value.X.As(DefaultUnits.StrainUnitResult), 4)}{strainUnitAbbreviation}, {Math.Round(Value.YY.As(DefaultUnits.CurvatureUnit), 4)}{curvatureUnitAbbreviation}, {Math.Round(Value.ZZ.As(DefaultUnits.CurvatureUnit), 4)}{curvatureUnitAbbreviation}}}";
+    }
+
+    public override bool CastTo<Q>(ref Q target) {
+      if (typeof(Q) == typeof(GH_Vector)) {
+        target = (Q)(object)new GH_Vector() {
+          Value = new Vector3d(Value.X.As(DefaultUnits.StrainUnitResult), Value.YY.As(DefaultUnits.CurvatureUnit),
+            Value.ZZ.As(DefaultUnits.CurvatureUnit))
+        };
+      }
+
+      return true;
     }
   }
 }
