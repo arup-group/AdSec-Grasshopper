389bb07378c68022946b10d3d675b3db740f2c93
Sandeep Kumar <sandeep.kumar@arup.com>
2026-07-21 15:24:44 +0530

    preload count ok...just for test

--- AdSecCoreTests/FlattenRebarTests.cs
+++ AdSecCoreTests/FlattenRebarTests.cs
@@ -88,6 +88,13 @@ namespace AdSecCoreTests {
     }
 
     [Fact]
+    public void Preloadcountok() {
+      singleBars.Preload = IPreStress.Create(Pressure.FromMegapascals(1));
+      operation.Compute();
+      Assert.Single(operation.PreLoad.Value);
+    }
+
+    [Fact]
     public void ShouldAddOutputsAsManyMaterialsAsRebars() {
       Assert.Single(operation.Material.Value);
     }
